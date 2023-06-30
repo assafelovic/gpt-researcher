7cd6e3fe5e9e6dc2fe58c572afb77f8605026b46
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-06-30 13:01:36 -0400

    Added outline_report, resource_report. updated selenium library. commented function descriptions

--- agent/run.py
+++ agent/run.py
@@ -1,13 +1,22 @@
+# Description: This file is used to run the agent. It is the entry point for the agent.
 from agent.research_assistant import ResearchAssistant
 import datetime
 
+
 def run_agent(task):
+    """ Runs the agent with the given task.
+    Args: task (str): The task to run the agent with
+    Returns: None
+    """
+
     start_time = datetime.datetime.now()
     print("Start time: ", start_time)
 
     assistant = ResearchAssistant(task)
     assistant.conduct_research()
     assistant.write_report()
+    assistant.write_resource_report()
+    assistant.write_outline_report()
 
     end_time = datetime.datetime.now()
     print("End time: ", end_time)
