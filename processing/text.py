cc07d31cbd4ad215659cbd17cbefe57ef72c9835
assafelovic <assaf.elovic@gmail.com>
2023-07-01 15:44:26 +0300

    modified saved file name by report type

--- processing/text.py
+++ processing/text.py
@@ -144,7 +144,7 @@ def write_to_file(filename: str, text: str) -> None:
         file.write(text)
 
 def write_md_to_pdf(task: str, directory_name: str, text: str) -> None:
-    file_path = f"./outputs/{directory_name}/resource_report"
+    file_path = f"./outputs/{directory_name}/{task}.pdf"
     write_to_file(f"{file_path}.md", text)
     md_to_pdf(f"{file_path}.md", f"{file_path}.pdf")
     print(f"{task} written to {file_path}.pdf")
