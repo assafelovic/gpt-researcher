7495e0774ac8b7debb677ee1bc8f2b883a9d7ba3
ElishaKay <kramer1346@gmail.com>
2023-07-27 20:39:46 +0000

    fixed report_type value getting passed from frontend to websocket

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -20,23 +20,21 @@ export default function ChatBox() {
     let {task, agent, report_type} = e.target;
 
     console.log(task.value, agent.value, report_type.value)
-    setTask(task.value)
-    setAgent(agent.value)
-    setReportType(report_type.value)
+    
     setAgentLogs([{output: "🤔 Thinking about research questions for the task..."}]);
-    startResearch(task, reportType, agent)
+    startResearch(task, report_type, agent)
   }
 
-  const startResearch = (task, reportType, agent) => {
+  const startResearch = (task, report_type, agent) => {
       // Clear output and reportContainer divs
 
     //   document.getElementById("output").innerHTML = "";
     //   document.getElementById("reportContainer").innerHTML = "";
 
-      listenToSockEvents(task, reportType, agent);
+      listenToSockEvents(task, report_type, agent);
   }
 
-  const listenToSockEvents = (task, reportType, agent) => {
+  const listenToSockEvents = (task, report_type, agent) => {
       // const {protocol, host, pathname} = window.location;
       // const ws_uri = `${protocol === 'https:' ? 'wss:' : 'ws:'}//${host}${pathname}ws`;
 
@@ -59,7 +57,7 @@ export default function ChatBox() {
         //   let task = document.querySelector('input[name="task"]').value;
         //   let report_type = document.querySelector('select[name="report_type"]').value;
         //   let agent = document.querySelector('input[name="agent"]:checked').value;  // Corrected line
-          let data = "start " + JSON.stringify({task: task.value, report_type: 'research_report', agent: agent.value});
+          let data = "start " + JSON.stringify({task: task.value, report_type: report_type.value, agent: agent.value});
           socket.send(data);
       };
   }
