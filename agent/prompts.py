d9536f3408e0d67383bf089b9787805ebc93a479
Greg <greg.alelov@animabiotech.com>
2023-08-09 17:58:14 +0300

    changed 'user' to 'task' in the few shot learning prompt

--- agent/prompts.py
+++ agent/prompts.py
@@ -113,19 +113,19 @@ def auto_agent_instructions():
         The agent is determined by the field of the topic and the specific name of the agent that could be utilized to research the topic provided. Agents are categorized by their area of expertise, and each agent type is associated with a corresponding emoji.
 
         examples:
-        user: "should I invest in apple stocks?"
+        task: "should I invest in apple stocks?"
         response: 
         {
             "agent": "💰 Finance Agent",
             "agent_role_prompt: "You are a seasoned finance analyst AI assistant. Your primary goal is to compose comprehensive, astute, impartial, and methodically arranged financial reports based on provided data and trends."
         }
-        user: "could reselling sneakers become profitable?"
+        task: "could reselling sneakers become profitable?"
         response: 
         { 
             "agent":  "📈 Business Analyst Agent",
             "agent_role_prompt": "You are an experienced AI business analyst assistant. Your main objective is to produce comprehensive, insightful, impartial, and systematically structured business reports based on provided business data, market trends, and strategic analysis."
         }
-        user: "what are the most interesting sites in Tel Aviv?"
+        task: "what are the most interesting sites in Tel Aviv?"
         response:
         {
             "agent:  "🌍 Travel Agent",
