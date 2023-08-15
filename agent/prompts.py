c5349fcd9bb9c8b70c8c2142d19b98587407ba19
Nicolas Alcaraz <nalcaraz@kvz.cl>
2023-08-15 18:38:46 -0500

    feat(prompts.py): add functions to generate agent role prompt and report prompt
    The `generate_agent_role_prompt` function generates a prompt based on the type of agent. It returns a string that describes the role and responsibilities of the agent.
    
    The `generate_report_prompt` function generates a prompt for writing a detailed report based on a given question and research summary. It returns a string that provides instructions for writing the report, including the required length, format, and content.
    
    These functions are added to the `prompts.py` file to enhance the functionality of the agent prompts module.
    
    chore(prompts.py): remove commented out code and unused functions
    docs(prompts.py): add docstrings to functions
    feat(prompts.py): add auto_agent_instructions function to provide instructions for different agent types
    
    fix(config.py): change default value of NUM_SEARCH_QUERIES from 8 to 10

--- agent/prompts.py
+++ agent/prompts.py
@@ -1,3 +1,34 @@
+def generate_agent_role_prompt(agent):
+    """ Generates the agent role prompt.
+    Args: agent (str): The type of the agent.
+    Returns: str: The agent role prompt.
+    """
+    prompts = {
+        "Finance Agent": "You are a seasoned finance analyst AI assistant. Your primary goal is to compose comprehensive, astute, impartial, and methodically arranged financial reports based on provided data and trends.",
+        "Travel Agent": "You are a world-travelled AI tour guide assistant. Your main purpose is to draft engaging, insightful, unbiased, and well-structured travel reports on given locations, including history, attractions, and cultural insights.",
+        "Academic Research Agent": "You are an AI academic research assistant. Your primary responsibility is to create thorough, academically rigorous, unbiased, and systematically organized reports on a given research topic, following the standards of scholarly work.",
+        "Business Analyst": "You are an experienced AI business analyst assistant. Your main objective is to produce comprehensive, insightful, impartial, and systematically structured business reports based on provided business data, market trends, and strategic analysis.",
+        "Computer Security Analyst Agent": "You are an AI specializing in computer security analysis. Your principal duty is to generate comprehensive, meticulously detailed, impartial, and systematically structured reports on computer security topics. This includes Exploits, Techniques, Threat Actors, and Advanced Persistent Threat (APT) Groups. All produced reports should adhere to the highest standards of scholarly work and provide in-depth insights into the complexities of computer security.",
+        "Default Agent": "You are an AI critical thinker research assistant. Your sole purpose is to write well written, critically acclaimed, objective and structured reports on given text."
+    }
+
+    return prompts.get(agent, "No such agent")
+
+
+def generate_report_prompt(question, research_summary):
+    """ Generates the report prompt for the given question and research summary.
+    Args: question (str): The question to generate the report prompt for
+            research_summary (str): The research summary to generate the report prompt for
+    Returns: str: The report prompt for the given question and research summary
+    """
+
+    return f'"""{research_summary}""" Using the above information, answer the following'\
+           f' question or topic: "{question}" in a detailed report --'\
+           " The report should focus on the answer to the question, should be well structured, informative," \
+           " in depth, with facts and numbers if available, a minimum of 1,200 words and with markdown syntax and apa format. "\
+            "You MUST determine your own concrete and valid opinion based on the information found. Do NOT deter to general and meaningless conclusions." \
+           "Write all source urls at the end of the report in apa format"
+
 def generate_search_queries_prompt(question):
     """ Generates the search queries prompt for the given question.
     Args: question (str): The question to generate the search queries prompt for
@@ -8,3 +39,108 @@ def generate_search_queries_prompt(question):
     queries_str = ', '.join(queries)
     return f'Write {num_search_queries} google search queries to search online that form an objective opinion from the following: "{question}"'\
            f'You must respond only with a list of strings in the following json format: {queries_str}'
+# def generate_search_queries_prompt(question):
+#     """ Generates the search queries prompt for the given question.
+#     Args: question (str): The question to generate the search queries prompt for
+#     Returns: str: The search queries prompt for the given question
+#     """
+
+#     return f'Write 5 google search queries to search online that form an objective opinion from the following: "{question}"'\
+#            f'You must respond only with a list of strings in the following json format: ["query 1", "query 2", "query 3", "query 4", "query 5"]'
+    # return f'Write 8 google search queries to search online that form an objective opinion from the following: "{question}"'\
+        #    f'You must respond only with a list of strings in the following json format: ["query 1", "query 2", "query 3", "query 4", "query 5", "query 6", "query 7", "query 8"]'
+
+
+def generate_resource_report_prompt(question, research_summary):
+    """Generates the resource report prompt for the given question and research summary.
+
+    Args:
+        question (str): The question to generate the resource report prompt for.
+        research_summary (str): The research summary to generate the resource report prompt for.
+
+    Returns:
+        str: The resource report prompt for the given question and research summary.
+    """
+    return f'"""{research_summary}""" Based on the above information, generate a bibliography recommendation report for the following' \
+           f' question or topic: "{question}". The report should provide a detailed analysis of each recommended resource,' \
+           ' explaining how each source can contribute to finding answers to the research question.' \
+           ' Focus on the relevance, reliability, and significance of each source.' \
+           ' Ensure that the report is well-structured, informative, in-depth, and follows Markdown syntax.' \
+           ' Include relevant facts, figures, and numbers whenever available.' \
+           ' The report should have a minimum length of 1,200 words.'
+
+
+def generate_outline_report_prompt(question, research_summary):
+    """ Generates the outline report prompt for the given question and research summary.
+    Args: question (str): The question to generate the outline report prompt for
+            research_summary (str): The research summary to generate the outline report prompt for
+    Returns: str: The outline report prompt for the given question and research summary
+    """
+
+    return f'"""{research_summary}""" Using the above information, generate an outline for a research report in Markdown syntax'\
+           f' for the following question or topic: "{question}". The outline should provide a well-structured framework'\
+           ' for the research report, including the main sections, subsections, and key points to be covered.' \
+           ' The research report should be detailed, informative, in-depth, and a minimum of 1,200 words.' \
+           ' Use appropriate Markdown syntax to format the outline and ensure readability.'
+
+def generate_concepts_prompt(question, research_summary):
+    """ Generates the concepts prompt for the given question.
+    Args: question (str): The question to generate the concepts prompt for
+            research_summary (str): The research summary to generate the concepts prompt for
+    Returns: str: The concepts prompt for the given question
+    """
+
+    return f'"""{research_summary}""" Using the above information, generate a list of 5 main concepts to learn for a research report'\
+           f' on the following question or topic: "{question}". The outline should provide a well-structured framework'\
+           'You must respond with a list of strings in the following format: ["concepts 1", "concepts 2", "concepts 3", "concepts 4, concepts 5"]'
+
+
+def generate_lesson_prompt(concept):
+    """
+    Generates the lesson prompt for the given question.
+    Args:
+        concept (str): The concept to generate the lesson prompt for.
+    Returns:
+        str: The lesson prompt for the given concept.
+    """
+
+    prompt = f'generate a comprehensive lesson about {concept} in Markdown syntax. This should include the definition'\
+    f'of {concept}, its historical background and development, its applications or uses in different'\
+    f'fields, and notable events or facts related to {concept}.'
+
+    return prompt
+
+def get_report_by_type(report_type):
+    report_type_mapping = {
+        'research_report': generate_report_prompt,
+        'resource_report': generate_resource_report_prompt,
+        'outline_report': generate_outline_report_prompt
+    }
+    return report_type_mapping[report_type]
+
+def auto_agent_instructions():
+    return """
+        This task involves researching a given topic, regardless of its complexity or the availability of a definitive answer. The research is conducted by a specific agent, defined by its type and role, with each agent requiring distinct instructions.
+        Agent
+        The agent is determined by the field of the topic and the specific name of the agent that could be utilized to research the topic provided. Agents are categorized by their area of expertise, and each agent type is associated with a corresponding emoji.
+
+        examples:
+        task: "should I invest in apple stocks?"
+        response: 
+        {
+            "agent": "💰 Finance Agent",
+            "agent_role_prompt: "You are a seasoned finance analyst AI assistant. Your primary goal is to compose comprehensive, astute, impartial, and methodically arranged financial reports based on provided data and trends."
+        }
+        task: "could reselling sneakers become profitable?"
+        response: 
+        { 
+            "agent":  "📈 Business Analyst Agent",
+            "agent_role_prompt": "You are an experienced AI business analyst assistant. Your main objective is to produce comprehensive, insightful, impartial, and systematically structured business reports based on provided business data, market trends, and strategic analysis."
+        }
+        task: "what are the most interesting sites in Tel Aviv?"
+        response:
+        {
+            "agent:  "🌍 Travel Agent",
+            "agent_role_prompt": "You are a world-travelled AI tour guide assistant. Your main purpose is to draft engaging, insightful, unbiased, and well-structured travel reports on given locations, including history, attractions, and cultural insights."
+        }
+    """
