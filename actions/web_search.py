163afafc7b04ea3e53aa9f8efea43d3951763629
Nicolas Alcaraz <nalcaraz@kvz.cl>
2023-08-14 23:33:48 -0500

    fix(web_search.py): remove unnecessary async keyword from web_search function fix(web_search.py): handle error in getting vqd and return error message as JSON

--- actions/web_search.py
+++ actions/web_search.py
@@ -2,23 +2,22 @@ from __future__ import annotations
 import json
 from duckduckgo_search import DDGS
 
-async def web_search(query: str, num_results: int = 8) -> str:
+def web_search(query: str, num_results: int = 8) -> str:
     """Useful for general internet search queries."""
     print("Searching with query {0}...".format(query))
     search_results = []
-    if not query:
-        return json.dumps(search_results)
 
-    ddgs = DDGS()
     try:
-        results = await ddgs.text(query)
+        ddgs = DDGS()
+        results = ddgs.text(query)
     except AssertionError:
         # This is the error that we are trying to catch.
-        print("Ignoring error:", e)
+        traceback_str = f"Traceback (most recent call last):\n{repr(e)}"
+        print("Ignoring error:", traceback_str)
         return json.dumps(search_results)
 
-    if not results:
-        return json.dumps(search_results)
+    if results is None:
+        return json.dumps({"error": "error in getting vqd"})
 
     results = list(results)
 
@@ -28,5 +27,6 @@ async def web_search(query: str, num_results: int = 8) -> str:
         total_added += 1
         if total_added >= num_results:
             break
+
 
     return json.dumps(search_results, ensure_ascii=False, indent=4)
