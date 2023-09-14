9acd6327848eb4b5214ded1abc7b0bbdc2b89756
ElishaKay <kramer1346@gmail.com>
2023-09-14 20:08:54 +0300

    added rabbit.py - new class to be instantiated per task & research_agent.py changes

---
+++ agent/rabbit.py
@@ -0,0 +1,50 @@
+from agent import prompts
+import pika
+import uuid
+import json
+
+rabbitConnection = pika.BlockingConnection(pika.ConnectionParameters(blocked_connection_timeout=3600,heartbeat=3600,host='rabbit'))
+
+class RabbitTaskManager:
+    def __init__(self, task, agent, agent_role_prompt):
+        """ Initializes the Rabbit Task Manager (1 new Channel per Task) for the main question.
+        Args: question (str): The main question to research
+        Returns: None
+        """
+        self.question = task
+        self.agent = agent
+        self.agent_role_prompt = agent_role_prompt if agent_role_prompt else prompts.generate_agent_role_prompt(agent)
+        self.task_id = uuid.uuid4()
+        self.channel = None
+        self.exchange_name = 'gpt-researcher'
+        self.routing_key= 'logs'
+
+
+    async def create_channel(self):
+        global rabbitConnection
+        if not rabbitConnection or rabbitConnection.is_closed:
+            rabbitConnection = pika.BlockingConnection(pika.ConnectionParameters(blocked_connection_timeout=3600,heartbeat=3600,host='rabbit'))
+        
+        self.channel = rabbitConnection.channel()
+        # This will create the exchange if it doesn't already exist.
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='fanout', durable=True)
+
+
+    def publish_to_rabbit(self, message):
+        """ Publishes message to rabbitmq
+        Args: message (dict): message to publish
+            channel: rabbitmq channel created for the task
+        """
+        message["task_id"] = f"{self.task_id}"
+        message["question"] = self.question
+        message["agent"] = self.agent
+        message["agent_role_prompt"]= self.agent_role_prompt
+
+        self.channel.basic_publish(exchange=self.exchange_name,
+                                routing_key=self.routing_key,
+                                body=json.dumps(message),
+                                properties=pika.BasicProperties(
+                                    delivery_mode = 2,
+                                ))
+    
+    
--- agent/research_agent.py
+++ agent/research_agent.py
@@ -23,7 +23,7 @@ CFG = Config()
 
 
 class ResearchAgent:
-    def __init__(self, question, agent, agent_role_prompt, websocket):
+    def __init__(self, question, agent, agent_role_prompt, websocket, rabbit):
         """ Initializes the research assistant with the given question.
         Args: question (str): The question to research
         Returns: None
@@ -37,7 +37,7 @@ class ResearchAgent:
         self.directory_name = uuid.uuid4()
         self.dir_path = os.path.dirname(f"./outputs/{self.directory_name}/")
         self.websocket = websocket
-
+        self.rabbit = rabbit
 
     async def summarize(self, text, topic):
         """ Summarizes the given text for the given topic.
@@ -47,7 +47,10 @@ class ResearchAgent:
         """
 
         messages = [create_message(text, topic)]
-        await self.websocket.send_json({"type": "logs", "output": f"📝 Summarizing text for query: {text}"})
+
+        summary_text_logs = {"type": "logs", "output": f"📝 Summarizing text for query: {text}"}
+        self.rabbit.publish_to_rabbit(summary_text_logs)
+        await self.websocket.send_json(summary_text_logs)
 
         return create_chat_completion(
             model=CFG.fast_llm_model,
@@ -63,7 +66,9 @@ class ResearchAgent:
         new_urls = []
         for url in url_set_input:
             if url not in self.visited_urls:
-                await self.websocket.send_json({"type": "logs", "output": f"✅ Adding source url to research: {url}\n"})
+                adding_source_url_logs = {"type": "logs", "output": f"✅ Adding source url to research: {url}\n"}
+                self.rabbit.publish_to_rabbit(adding_source_url_logs)
+                await self.websocket.send_json(adding_source_url_logs)
                 self.visited_urls.add(url)
                 new_urls.append(url)
 
@@ -82,6 +87,7 @@ class ResearchAgent:
             messages=messages,
             stream=stream,
             websocket=websocket,
+            rabbit=self.rabbit
         )
         return answer
 
@@ -92,7 +98,11 @@ class ResearchAgent:
         """
         result = await self.call_agent(prompts.generate_search_queries_prompt(self.question))
         print(result)
-        await self.websocket.send_json({"type": "logs", "output": f"🧠 I will conduct my research based on the following queries: {result}..."})
+
+        research_queries_logs = {"type": "logs", "output": f"🧠 I will conduct my research based on the following queries: {result}..."}
+
+        self.rabbit.publish_to_rabbit(research_queries_logs)
+        await self.websocket.send_json(research_queries_logs)
         return json.loads(result)
 
     async def async_search(self, query):
@@ -102,12 +112,13 @@ class ResearchAgent:
         """
         search_results = json.loads(web_search(query))
         new_search_urls = self.get_new_urls([url.get("href") for url in search_results])
-
-        await self.websocket.send_json(
-            {"type": "logs", "output": f"🌐 Browsing the following sites for relevant information: {new_search_urls}..."})
+        
+        sites_to_browse_logs = {"type": "logs", "output": f"🌐 Browsing the following sites for relevant information: {new_search_urls}..."}
+        self.rabbit.publish_to_rabbit(sites_to_browse_logs)
+        await self.websocket.send_json(sites_to_browse_logs)
 
         # Create a list to hold the coroutine objects
-        tasks = [async_browse(url, query, self.websocket) for url in await new_search_urls]
+        tasks = [async_browse(url, query, self.websocket, self.rabbit) for url in await new_search_urls]
 
         # Gather the results as they become available
         responses = await asyncio.gather(*tasks, return_exceptions=True)
@@ -120,13 +131,18 @@ class ResearchAgent:
         Returns: str: The search summary for the given query
         """
 
-        await self.websocket.send_json({"type": "logs", "output": f"🔎 Running research for '{query}'..."})
+        query_to_research_logs = {"type": "logs", "output": f"🔎 Running research for '{query}'..."}
+        self.rabbit.publish_to_rabbit(query_to_research_logs)
+        await self.websocket.send_json(query_to_research_logs)
 
         responses = await self.async_search(query)
 
         result = "\n".join(responses)
-        os.makedirs(os.path.dirname(f"./outputs/{self.directory_name}/research-{query}.txt"), exist_ok=True)
-        write_to_file(f"./outputs/{self.directory_name}/research-{query}.txt", result)
+        result_logs = {"type": "final report", "output": result}
+        self.rabbit.publish_to_rabbit({"type": "final report", "output": result})
+        
+        # os.makedirs(os.path.dirname(f"./outputs/{self.directory_name}/research-{query}.txt"), exist_ok=True)
+        # write_to_file(f"./outputs/{self.directory_name}/research-{query}.txt", result)
         return result
 
     async def conduct_research(self):
@@ -143,8 +159,9 @@ class ResearchAgent:
                 research_result = await self.run_search_summary(query)
                 self.research_summary += f"{research_result}\n\n"
 
-        await self.websocket.send_json(
-            {"type": "logs", "output": f"Total research words: {len(self.research_summary.split(' '))}"})
+        research_summary_logs = {"type": "logs", "output": f"Total research words: {len(self.research_summary.split(' '))}"}
+        self.rabbit.publish_to_rabbit(research_summary_logs)
+        await self.websocket.send_json(research_summary_logs)
 
         return self.research_summary
 
@@ -155,8 +172,10 @@ class ResearchAgent:
         Returns: list[str]: The concepts for the given question
         """
         result = self.call_agent(prompts.generate_concepts_prompt(self.question, self.research_summary))
-
-        await self.websocket.send_json({"type": "logs", "output": f"I will research based on the following concepts: {result}\n"})
+        
+        concepts_to_research_logs = {"type": "logs", "output": f"I will research based on the following concepts: {result}\n"}
+        self.rabbit.publish_to_rabbit(concepts_to_research_logs)
+        await self.websocket.send_json(concepts_to_research_logs)
         return json.loads(result)
 
     async def write_report(self, report_type, websocket):
@@ -165,8 +184,11 @@ class ResearchAgent:
         Returns: str: The report for the given question
         """
         report_type_func = prompts.get_report_by_type(report_type)
-        await websocket.send_json(
-            {"type": "logs", "output": f"✍️ Writing {report_type} for research task: {self.question}..."})
+        
+        writing_report_logs = {"type": "logs", "output": f"✍️ Writing {report_type} for research task: {self.question}..."}
+
+        self.rabbit.publish_to_rabbit(writing_report_logs)
+        await websocket.send_json(writing_report_logs)
         answer = await self.call_agent(report_type_func(self.question, self.research_summary), stream=True,
                                        websocket=websocket)
 
