392401e226e2758483d482540c3b433d48579ec8
assafelovic <assaf.elovic@gmail.com>
2023-09-16 20:54:16 +0300

    cleaned code

--- permchain_example/writer_actors/writer.py
+++ permchain_example/writer_actors/writer.py
@@ -10,15 +10,18 @@ class WriterActor:
         self.model = ChatOpenAI(model=CFG.smart_llm_model)
         self.prompt = ChatPromptTemplate.from_messages([
             ("system",
-             "You are an AI critical thinker research assistant. Your sole purpose is to write well written, critically acclaimed, objective and structured reports on given text."
+             "You are an AI critical thinker research assistant. "
+             "Your sole purpose is to write well written, critically acclaimed, objective and structured reports on given text."
              ),
             ("user",
              '"""{results}""" Using the above information, answer the following'
-                       ' question or topic: "{query}" in a detailed report --'
-                       " The report should focus on the answer to the question, should be well structured, informative,"
-                       " in depth, with facts and numbers if available, a minimum of 1,200 words and with markdown syntax (including titles) and apa format. "
-                       "You MUST determine your own concrete and valid opinion based on the given information. Do NOT deter to general and meaningless conclusions."
-                       "Write all used source urls at the end of the report in apa format"
+               ' question or topic: "{query}" in a detailed report --'
+               " The report should focus on the answer to the question, should be well structured, informative,"
+               " in depth, with facts and numbers if available, "
+               "a minimum of 1,200 words and with markdown syntax (including titles) and apa format. "
+               "You MUST determine your own concrete and valid opinion based on the given information. "
+               "Do NOT deter to general and meaningless conclusions."
+               "Write all used source urls at the end of the report in apa format"
              )
         ])
 
