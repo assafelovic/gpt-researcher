7893e00d9ff24481e98398c616f52e33850db39b
assafelovic <assaf.elovic@gmail.com>
2023-09-16 20:21:03 +0300

    added new actors and formed a research team

--- permchain_example/test.py
+++ permchain_example/test.py
@@ -4,14 +4,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from permchain_example.researcher import Researcher
 from permchain_example.search_actors.search_api import TavilySearchActor
+from permchain_example.editor_actors.editor import EditorActor
+from permchain_example.reviser_actors.reviser import ReviserActor
 from permchain_example.search_actors.gpt_researcher import GPTResearcherActor
-from permchain_example.writer_actors.openai import OpenAIWriterActor
+from permchain_example.writer_actors.writer import WriterActor
+from permchain_example.research_team import ResearchTeam
 from processing.text import md_to_pdf
 
 
 
 if __name__ == '__main__':
-    output_path = "./permchain_example/output"
+    output_path = "./output"
     if not os.path.exists(output_path):
         # If the directory does not exist, create it
         os.makedirs(output_path)
@@ -19,10 +22,10 @@ if __name__ == '__main__':
     stocks = ["AAPL"]
 
     for stock in stocks[:1]:
-        researcher = Researcher(GPTResearcherActor(), OpenAIWriterActor())
-        research = researcher.run(f"is the stock {stock} a good buy?")
+        researcher = Researcher(GPTResearcherActor(), WriterActor())
+        research_team = ResearchTeam(researcher, EditorActor(), ReviserActor())
+
+        draft = research_team.run(f"is the stock {stock} a good buy?")
         with open(f"{output_path}/{stock}.md", "w") as f:
-            f.write(research)
+            f.write(draft)
         md_to_pdf(f"{output_path}/{stock}.md", f"{output_path}/{stock}.pdf")
-
-
