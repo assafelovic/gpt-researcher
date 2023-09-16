7931884bb3d312ed38b132d51d0c956665a6c91f
assafelovic <assaf.elovic@gmail.com>
2023-09-16 20:31:19 +0300

    wrapped editor in object

--- permchain_example/editor_actors/editor.py
+++ permchain_example/editor_actors/editor.py
@@ -10,46 +10,42 @@ Please accept the draft if it is good enough to publish, or send it for revision
 Things you should be checking for:
 
 - This draft MUST fully answer the original question
-- This draft MUST be apa format
+- This draft MUST be written in apa format
 
 If not all of the above criteria are met, you should send appropriate revision notes.
 """
 
-critique_prompt = (
-    SystemMessagePromptTemplate.from_template(EDIT_TEMPLATE)
-    + "Draft:\n\n{draft}"
-)
-critique_llm = ChatOpenAI(model=CFG.smart_llm_model)
-functions = [
-    {
-        "name": "revise",
-        "description": "Sends the draft for revision",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "notes": {
-                    "type": "string",
-                    "description": "The editor's notes to guide the revision.",
-                },
-            },
-        },
-    },
-    {
-        "name": "accept",
-        "description": "Accepts the draft",
-        "parameters": {
-            "type": "object",
-            "properties": {"ready": {"const": True}},
-        },
-    },
-]
-
-#EditorActor = critique_prompt | critique_llm.bind(functions=functions)
-
 
 class EditorActor:
+    def __init__(self):
+        self.model = ChatOpenAI(model=CFG.smart_llm_model)
+        self.prompt = SystemMessagePromptTemplate.from_template(EDIT_TEMPLATE) + "Draft:\n\n{draft}"
+        self.functions = [
+            {
+                "name": "revise",
+                "description": "Sends the draft for revision",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "notes": {
+                            "type": "string",
+                            "description": "The editor's notes to guide the revision.",
+                        },
+                    },
+                },
+            },
+            {
+                "name": "accept",
+                "description": "Accepts the draft",
+                "parameters": {
+                    "type": "object",
+                    "properties": {"ready": {"const": True}},
+                },
+            },
+        ]
+
     @property
     def runnable(self):
         return (
-                critique_prompt | critique_llm.bind(functions=functions)
+            self.prompt | self.model.bind(functions=self.functions)
         )
