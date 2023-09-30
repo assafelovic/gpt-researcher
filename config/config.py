de68d8b8ce97ba45cb19de4df1f777ea1afa1590
assafelovic <assaf.elovic@gmail.com>
2023-09-30 11:05:19 +0300

    modified config params for improved performance

--- config/config.py
+++ config/config.py
@@ -24,8 +24,8 @@ class Config(metaclass=Singleton):
         self.llm_provider = os.getenv("LLM_PROVIDER", "ChatOpenAI")
         self.fast_llm_model = os.getenv("FAST_LLM_MODEL", "gpt-3.5-turbo-16k")
         self.smart_llm_model = os.getenv("SMART_LLM_MODEL", "gpt-4")
-        self.fast_token_limit = int(os.getenv("FAST_TOKEN_LIMIT", 4000))
-        self.smart_token_limit = int(os.getenv("SMART_TOKEN_LIMIT", 8000))
+        self.fast_token_limit = int(os.getenv("FAST_TOKEN_LIMIT", 2000))
+        self.smart_token_limit = int(os.getenv("SMART_TOKEN_LIMIT", 4000))
         self.browse_chunk_max_length = int(os.getenv("BROWSE_CHUNK_MAX_LENGTH", 8192))
         self.summary_token_limit = int(os.getenv("SUMMARY_TOKEN_LIMIT", 700))
 
