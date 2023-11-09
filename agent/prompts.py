bb3acc53663dd3b2f18aff9967594967461f15b3
assafelovic <assaf.elovic@gmail.com>
2023-11-09 09:48:05 +0200

    added dynamic total words for report

--- agent/prompts.py
+++ agent/prompts.py
@@ -16,7 +16,7 @@ def generate_agent_role_prompt(agent):
     return prompts.get(agent, "No such agent")
 
 
-def generate_report_prompt(question, research_summary, report_format="apa"):
+def generate_report_prompt(question, research_summary, report_format="apa", report_length=1500):
     """ Generates the report prompt for the given question and research summary.
     Args: question (str): The question to generate the report prompt for
             research_summary (str): The research summary to generate the report prompt for
@@ -27,7 +27,7 @@ def generate_report_prompt(question, research_summary, report_format="apa"):
            f'Using the above information, answer the following'\
            f' question or topic: "{question}" in a detailed report --'\
            " The report should focus on the answer to the question, should be well structured, informative," \
-           " in depth, with facts and numbers if available and a minimum of 1,200 words.\n" \
+           f" in depth, with facts and numbers if available and a minimum of {report_length} words.\n" \
            "You should strive to write the report as long as you can using all relevant and necessary information provided.\n" \
            "You must write the report with markdown syntax.\n "\
             "You MUST determine your own concrete and valid opinion based on the given information. Do NOT deter to general and meaningless conclusions.\n" \
