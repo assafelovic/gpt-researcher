51b6dd0597f7328846d21113799a14d9ef21b3bf
assafelovic <assaf.elovic@gmail.com>
2023-11-10 21:39:52 +0200

    Deploy website - based on f1dc084c9810de4941a78c2f0ea69121ce3a8792

---
+++ assets/js/031793e1.faf6ab72.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[633],{2511:e=>{e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/facebook","name":"facebook","count":1,"permalink":"/blog/tags/facebook"}')}}]);
---
+++ assets/js/814f3328.a82ecca6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
