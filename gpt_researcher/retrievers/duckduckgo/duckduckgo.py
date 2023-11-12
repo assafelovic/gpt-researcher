ff51358425ac7ff0f36d3e18fd5fae8fabb7a192
gregdrizz <gregalelov@gmail.com>
2023-11-12 22:07:56 +0200

    changed the duckduckgo class to work in the same way the other search classes work

--- gpt_researcher/retrievers/duckduckgo/duckduckgo.py
+++ gpt_researcher/retrievers/duckduckgo/duckduckgo.py
@@ -6,16 +6,17 @@ class Duckduckgo:
     """
     Duckduckgo API Retriever
     """
-    def __init__(self):
+    def __init__(self, query):
         self.ddg = DDGS()
+        self.query = query
 
-    def search(self, query, max_results=5):
+    def search(self, max_results=5):
         """
         Performs the search
         :param query:
         :param max_results:
         :return:
         """
-        ddgs_gen = self.ddg.text(query, region='wt-wt')
+        ddgs_gen = self.ddg.text(self.query, region='wt-wt')
         urls = [r["href"] for r in islice(ddgs_gen, max_results)]
         return urls
