e476c801f8400c11049056efc55310ed8b7c3a69
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-11-13 14:11:37 +0200

    Fix websocket bug

--- gpt_researcher/utils/websocket_manager.py
+++ gpt_researcher/utils/websocket_manager.py
@@ -16,11 +16,17 @@ class WebSocketManager:
 
     async def start_sender(self, websocket: WebSocket):
         """Start the sender task."""
-        queue = self.message_queues[websocket]
+        queue = self.message_queues.get(websocket)
+        if not queue:
+            return
+
         while True:
             message = await queue.get()
             if websocket in self.active_connections:
-                await websocket.send_text(message)
+                try:
+                    await websocket.send_text(message)
+                except:
+                    break
             else:
                 break
 
@@ -33,10 +39,12 @@ class WebSocketManager:
 
     async def disconnect(self, websocket: WebSocket):
         """Disconnect a websocket."""
-        self.active_connections.remove(websocket)
-        self.sender_tasks[websocket].cancel()
-        del self.sender_tasks[websocket]
-        del self.message_queues[websocket]
+        if websocket in self.active_connections:
+            self.active_connections.remove(websocket)
+            self.sender_tasks[websocket].cancel()
+            await self.message_queues[websocket].put(None)
+            del self.sender_tasks[websocket]
+            del self.message_queues[websocket]
 
     async def start_streaming(self, task, report_type, websocket):
         """Start streaming the output."""
