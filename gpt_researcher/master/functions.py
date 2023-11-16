31737ac599e5465f0963462026da2b50986e5089
assafelovic <assaf.elovic@gmail.com>
2023-11-16 13:18:01 +0200

    added ability to select iterations of research steps

--- gpt_researcher/master/functions.py
+++ gpt_researcher/master/functions.py
@@ -76,11 +76,12 @@ async def get_sub_queries(query, agent_role_prompt, cfg):
         sub_queries: List of sub queries
 
     """
+    max_research_iterations = cfg.max_iterations if cfg.max_iterations else 1
     response = await create_chat_completion(
         model=cfg.smart_llm_model,
         messages=[
             {"role": "system", "content": f"{agent_role_prompt}"},
-            {"role": "user", "content": generate_search_queries_prompt(query)}],
+            {"role": "user", "content": generate_search_queries_prompt(query, max_iterations=max_research_iterations)}],
         temperature=0,
         llm_provider=cfg.llm_provider
     )
