545691ca2f2348510124842b8db89903b7f3be12
assafelovic <assaf.elovic@gmail.com>
2023-11-17 16:51:38 +0200

    refactored for memory to be in agent

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -1,8 +1,11 @@
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 
-# Here will be all memory logic for holding a meaningful iterative conversation about multiple research tasks
+
 class Memory():
     def __init__(self, **kwargs):
         self._embeddings = OpenAIEmbeddings()
+
+    def get_embeddings(self):
+        return self._embeddings
 
