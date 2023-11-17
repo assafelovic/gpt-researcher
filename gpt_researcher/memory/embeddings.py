51563b60413ce049731fa8df2733dd9831afc485
assafelovic <assaf.elovic@gmail.com>
2023-11-17 16:52:10 +0200

    modified

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -2,7 +2,7 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 
 
-class Memory():
+class Memory:
     def __init__(self, **kwargs):
         self._embeddings = OpenAIEmbeddings()
 
