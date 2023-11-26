859342ff9d47154a8facfc32800500eec8863ca1
Rotem B. Weiss <91344214+rotemweiss57@users.noreply.github.com>
2023-11-26 13:18:30 +0200

    fix pip structure

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open(r"README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 with open("requirements.txt", "r") as f:
-    reqs = [line.strip() for line in f if ('selenium' not in line and 'webdriver' not in line and 'fastapi' not in line)]
+    reqs = [line.strip() for line in f if ('selenium' not in line and 'webdriver' not in line)]
 
 setup(
     name="gpt-researcher",
-    version="0.0.1",
+    version="0.0.5",
     description="GPT Researcher is an autonomous agent designed for comprehensive online research on a variety of tasks.",
-    package_dir={"": "gpt_researcher"},
-    packages=find_packages(where="gpt_researcher"),
+    package_dir={'gpt_researcher': 'gpt_researcher'},
+    packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/assafelovic/gpt-researcher",
