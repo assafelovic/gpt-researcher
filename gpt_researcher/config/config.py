6af468b16a8ff91d9af5f3ec89bb512d5cf04684
assafelovic <assaf.elovic@gmail.com>
2023-12-05 18:04:22 +0200

    added env to config

--- gpt_researcher/config/config.py
+++ gpt_researcher/config/config.py
@@ -1,5 +1,6 @@
 # config file
 import json
+import os
 
 
 class Config:
@@ -7,23 +8,23 @@ class Config:
 
     def __init__(self, config_file: str = None):
         """Initialize the config class."""
-        self.config_file = config_file
-        self.retriever = "tavily"
-        self.llm_provider = "ChatOpenAI"
-        self.fast_llm_model = "gpt-3.5-turbo-16k"
-        self.smart_llm_model = "gpt-4-1106-preview"
-        self.fast_token_limit = 2000
-        self.smart_token_limit = 4000
-        self.browse_chunk_max_length = 8192
-        self.summary_token_limit = 700
-        self.temperature = 0.55
-        self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)" \
-                          " Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
-        self.max_search_results_per_query = 5
-        self.memory_backend = "local"
-        self.total_words = 1000
-        self.report_format = "apa"
-        self.max_iterations = 3
+        self.config_file = config_file if config_file else os.getenv('CONFIG_FILE')
+        self.retriever = os.getenv('SEARCH_RETRIEVER', "tavily")
+        self.llm_provider = os.getenv('LLM_PROVIDER', "ChatOpenAI")
+        self.fast_llm_model = os.getenv('FAST_LLM_MODEL', "gpt-3.5-turbo-16k")
+        self.smart_llm_model = os.getenv('SMART_LLM_MODEL', "gpt-4-1106-preview")
+        self.fast_token_limit = int(os.getenv('FAST_TOKEN_LIMIT', 2000))
+        self.smart_token_limit = int(os.getenv('SMART_TOKEN_LIMIT', 4000))
+        self.browse_chunk_max_length = int(os.getenv('BROWSE_CHUNK_MAX_LENGTH', 8192))
+        self.summary_token_limit = int(os.getenv('SUMMARY_TOKEN_LIMIT', 700))
+        self.temperature = float(os.getenv('TEMPERATURE', 0.55))
+        self.user_agent = os.getenv('USER_AGENT', "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                                                   "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0")
+        self.max_search_results_per_query = int(os.getenv('MAX_SEARCH_RESULTS_PER_QUERY', 5))
+        self.memory_backend = os.getenv('MEMORY_BACKEND', "local")
+        self.total_words = int(os.getenv('TOTAL_WORDS', 1000))
+        self.report_format = os.getenv('REPORT_FORMAT', "APA")
+        self.max_iterations = int(os.getenv('MAX_ITERATIONS', 3))
 
         self.load_config_file()
 
