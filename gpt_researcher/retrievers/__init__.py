cbc0585de19a60ca0dd1938b50f3a71fbbda28c0
assafelovic <assaf.elovic@gmail.com>
2023-12-09 15:01:39 +0200

    added tavily news retriever

--- gpt_researcher/retrievers/__init__.py
+++ gpt_researcher/retrievers/__init__.py
@@ -1,4 +1,5 @@
 from .tavily_search.tavily_search import TavilySearch
+from .tavily_news.tavily_news import TavilyNews
 from .duckduckgo.duckduckgo import Duckduckgo
 from .google.google import GoogleSearch
 from .serper.serper import SerperSearch
@@ -6,4 +7,13 @@ from .serpapi.serpapi import SerpApiSearch
 from .searx.searx import SearxSearch
 from .bing.bing import BingSearch
 
-__all__ = ["TavilySearch", "Duckduckgo", "SerperSearch", "SerpApiSearch", "GoogleSearch", "SearxSearch", "BingSearch"]
+__all__ = [
+    "TavilySearch",
+    "TavilyNews",
+    "Duckduckgo",
+    "SerperSearch",
+    "SerpApiSearch",
+    "GoogleSearch",
+    "SearxSearch",
+    "BingSearch"
+]
