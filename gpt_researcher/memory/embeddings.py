79ef162204367c202cdd5472d316436fe32b4ca6
jimmylin0979 <jimmylin0979@gmail.com>
2024-03-05 12:55:19 +0800

    Add support for various embedding provider

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -1,11 +1,28 @@
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
 
 
 class Memory:
-    def __init__(self, **kwargs):
-        self._embeddings = OpenAIEmbeddings()
+    def __init__(self, embedding_provider, **kwargs):
+
+        _embeddings = None
+        match embedding_provider:
+            case "ollama":
+                from langchain_community.embeddings import OllamaEmbeddings
+                _embeddings = OllamaEmbeddings(model="llama2")
+            case "openai":
+                from langchain.embeddings import OpenAIEmbeddings
+                _embeddings = OpenAIEmbeddings()
+            case "huggingface":
+                from langchain_community.embeddings import HuggingFaceEmbeddings
+                _embeddings = HuggingFaceEmbeddings()
+            case "mixtral":
+                from langchain_mistralai import MistralAIEmbeddingscase
+                _embeddings = MistralAIEmbeddingscase
+
+            case _:
+                raise Exception("Embedding provider not found.")
+
+        self._embeddings = _embeddings
 
     def get_embeddings(self):
         return self._embeddings
-
