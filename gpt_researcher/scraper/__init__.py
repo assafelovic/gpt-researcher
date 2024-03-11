7df09d413b25751ab7b17e68da988b98cd1df8ad
Priyadarshi Roy <proy9714@gmail.com>
2024-03-11 14:34:50 +0530

    Refactored each scraper into its own folder.
    Also included a new scraper from langchain_community: WebBaseLoader. WebBaseLoader works quite fast.
    
    Support for more such loaders can be integrated in the future with a similar interface.

--- gpt_researcher/scraper/__init__.py
+++ gpt_researcher/scraper/__init__.py
@@ -1,3 +1,15 @@
 from .scraper import Scraper
+from .beautiful_soup.beautiful_soup import BeautifulSoupScraper
+from .newspaper.newspaper import NewspaperScraper
+from .web_base_loader.web_base_loader import WebBaseLoaderScraper
+from .arxiv.arxiv import ArxivScraper
+from .pymupdf.pymupdf import PyMuPDFScraper
 
-__all__ = ["Scraper"]
+__all__ = [
+    "Scraper",
+    "BeautifulSoupScraper",
+    "NewspaperScraper",
+    "WebBaseLoaderScraper",
+    "ArxivScraper",
+    "PyMuPDFScraper"
+]
