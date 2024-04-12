20f11d4b0a4f4b0e3baee24cf9aa2692fdb955c4
assafelovic <assaf.elovic@gmail.com>
2024-04-12 15:35:18 +0300

    small refactoring

--- gpt_researcher/utils/enum.py
+++ gpt_researcher/utils/enum.py
@@ -14,13 +14,19 @@ class ReportType(Enum):
     DetailedReport = 'detailed_report'
     SubtopicReport = 'subtopic_report'
 
+report_type_mapping = {
+    ReportType.ResearchReport.value: generate_report_prompt,
+    ReportType.ResourceReport.value: generate_resource_report_prompt,
+    ReportType.OutlineReport.value: generate_outline_report_prompt,
+    ReportType.CustomReport.value: generate_custom_report_prompt,
+    ReportType.SubtopicReport.value: generate_subtopic_report_prompt
+}
+
 
 def get_prompt_by_report_type(report_type):
-    report_type_mapping = {
-        ReportType.ResearchReport.value: generate_report_prompt,
-        ReportType.ResourceReport.value: generate_resource_report_prompt,
-        ReportType.OutlineReport.value: generate_outline_report_prompt,
-        ReportType.CustomReport.value: generate_custom_report_prompt,
-        ReportType.SubtopicReport.value: generate_subtopic_report_prompt
-    }
-    return report_type_mapping[report_type]
+    default_report_type = ReportType.ResearchReport.value
+    prompt_by_type = report_type_mapping.get(report_type)
+    if not prompt_by_type:
+        prompt_by_type = report_type_mapping.get(default_report_type)
+        print(f"Invalid report type: {report_type}. Using default report type: {default_report_type}")
+    return prompt_by_type
