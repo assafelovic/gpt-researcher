8dd0f4f399721fc2b016b22390f32f60cb8e567f
assafelovic <assaf.elovic@gmail.com>
2024-04-12 15:26:20 +0300

    refactored code to be better structured

--- gpt_researcher/utils/enum.py
+++ gpt_researcher/utils/enum.py
@@ -1,4 +1,10 @@
 from enum import Enum
+from gpt_researcher.master.prompts import \
+    generate_report_prompt, \
+    generate_resource_report_prompt, \
+    generate_outline_report_prompt, \
+    generate_custom_report_prompt, \
+    generate_subtopic_report_prompt
 
 class ReportType(Enum):
     ResearchReport = 'research_report'
@@ -7,3 +13,14 @@ class ReportType(Enum):
     CustomReport = 'custom_report'
     DetailedReport = 'detailed_report'
     SubtopicReport = 'subtopic_report'
+
+
+def get_prompt_by_report_type(report_type):
+    report_type_mapping = {
+        ReportType.ResearchReport.value: generate_report_prompt,
+        ReportType.ResourceReport.value: generate_resource_report_prompt,
+        ReportType.OutlineReport.value: generate_outline_report_prompt,
+        ReportType.CustomReport.value: generate_custom_report_prompt,
+        ReportType.SubtopicReport.value: generate_subtopic_report_prompt
+    }
+    return report_type_mapping[report_type]
