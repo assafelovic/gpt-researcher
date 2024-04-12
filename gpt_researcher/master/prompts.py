28460df04c100a6250b952ed867fd6702cc1ab92
assafelovic <assaf.elovic@gmail.com>
2024-04-12 16:39:42 +0300

    modified warning for invalid prompt usage

--- gpt_researcher/master/prompts.py
+++ gpt_researcher/master/prompts.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-
+import warnings
 from gpt_researcher.utils.enum import ReportType
 
 
@@ -233,7 +233,11 @@ report_type_mapping = {
 
 def get_prompt_by_report_type(report_type):
     prompt_by_type = report_type_mapping.get(report_type)
+    default_report_type = ReportType.ResearchReport.value
     if not prompt_by_type:
-        raise Exception(f"Invalid report type: {report_type}.\n"
-                        f"Please use one of the following: {', '.join(report.value for report in ReportType)}")
+        warnings.warn(f"Invalid report type: {report_type}.\n"
+                        f"Please use one of the following: {', '.join(report.value for report in ReportType)}\n"
+                        f"Using default report type: {default_report_type} prompt.",
+                        UserWarning)
+        prompt_by_type = report_type_mapping.get(default_report_type)
     return prompt_by_type
