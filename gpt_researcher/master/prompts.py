42677c5b6ad8475f41465686b41ecac2db56f310
assafelovic <assaf.elovic@gmail.com>
2024-04-12 17:17:28 +0300

    refactored code to be better structured

--- gpt_researcher/master/prompts.py
+++ gpt_researcher/master/prompts.py
@@ -236,7 +236,7 @@ def get_prompt_by_report_type(report_type):
     default_report_type = ReportType.ResearchReport.value
     if not prompt_by_type:
         warnings.warn(f"Invalid report type: {report_type}.\n"
-                        f"Please use one of the following: {', '.join(report.value for report in ReportType)}\n"
+                        f"Please use one of the following: {', '.join([enum_value for enum_value in report_type_mapping.keys()])}\n"
                         f"Using default report type: {default_report_type} prompt.",
                         UserWarning)
         prompt_by_type = report_type_mapping.get(default_report_type)
