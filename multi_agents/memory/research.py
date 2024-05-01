df459d55073e8b26b6cf0bb7bf528ccdc46e0075
assafelovic <assaf.elovic@gmail.com>
2024-05-01 19:02:47 +0300

    added persistency

---
+++ multi_agents/memory/research.py
@@ -0,0 +1,18 @@
+from typing import TypedDict, List, Annotated
+import operator
+
+
+class ResearchState(TypedDict):
+    task: dict
+    initial_research: str
+    subheaders: List[str]
+    title: str
+    date: str
+    table_of_contents: str
+    introduction: str
+    research_data: List[dict]
+    conclusion: str
+    sources: List[str]
+    report: str
+
+
