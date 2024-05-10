def0c8dfe9a5d594fe7dc4aa25fc5d5d934f90ae
assafelovic <assaf.elovic@gmail.com>
2024-05-10 08:36:33 +0300

    added task validation at init

--- multi_agents/main.py
+++ multi_agents/main.py
@@ -10,9 +10,18 @@ if os.environ.get("LANGCHAIN_API_KEY"):
 load_dotenv()
 
 
-async def main():
+def open_task():
     with open('task.json', 'r') as f:
         task = json.load(f)
+
+    if not task:
+        raise Exception("No task provided. Please include a task.json file in the root directory.")
+
+    return task
+
+
+async def main():
+    task = open_task()
 
     chief_editor = ChiefEditorAgent(task)
     research_report = await chief_editor.run_research_task()
