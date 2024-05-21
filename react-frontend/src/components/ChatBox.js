c6ea061455d20d22f9e9c9a5a41956e20c97ee30
ElishaKay <kramer1346@gmail.com>
2024-05-21 21:56:00 +0300

    Dockerfile.dev supports hot reloading while running react app from docker

--- react-frontend/src/components/ChatBox.js
+++ react-frontend/src/components/ChatBox.js
@@ -47,8 +47,8 @@ export default function ChatBox() {
       <section className="landing">
           <div className="max-w-5xl mx-auto text-center">
               <h1 className="text-4xl font-extrabold mx-auto lg:text-7xl">
-                  Say Hello to
-                  <span className="sayGoodbye"> GPT Researcher</span>
+                Say Goodbye to 
+                  <span className="sayGoodbye"> Hours of Research</span>
               </h1>
               <p className="max-w-5xl mx-auto text-gray-600 mt-8" style={{fontSize: "20px"}}>
                   The power of Tavily Search & GPT Combined for a Better Search Experience.
