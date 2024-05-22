14631ed96a202e4dbf5d09d0c5fd0d23cc568c32
Guido Schmutz <guido.schmutz@trivadis.com>
2024-05-22 21:45:31 +0200

    Adding support for Ollama (both LLM and embeddings)

--- gpt_researcher/llm_provider/__init__.py
+++ gpt_researcher/llm_provider/__init__.py
@@ -1,9 +1,11 @@
 from .google.google import GoogleProvider
 from .openai.openai import OpenAIProvider
 from .azureopenai.azureopenai import AzureOpenAIProvider
+from .ollama.ollama import OllamaProvider
 
 __all__ = [
     "GoogleProvider",
     "OpenAIProvider",
-    "AzureOpenAIProvider"
+    "AzureOpenAIProvider",
+    "OllamaProvider"
 ]
