f373b71d126293a98f6fd1c3927bb5fecbf15952
sebaxzero <ncsebaxzero@gmail.com>
2024-05-22 17:29:54 -0400

    add support for local embeddings with openai api

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -12,7 +12,17 @@ class Memory:
                 _embeddings = OllamaEmbeddings(model="llama2")
             case "openai":
                 from langchain_openai import OpenAIEmbeddings
-                _embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+                base_url = os.environ.get("OPENAI_BASE_URL", None)
+                api_key = os.environ["OPENAI_API_KEY"]
+                model = os.environ.get("EMB_MODEL", "local")
+                if base_url:
+                    _embeddings = OpenAIEmbeddings(model=model,
+                                                   openai_api_key=api_key, 
+                                                   openai_api_base=base_url,
+                                                   check_embedding_ctx_length=False, #quick fix for lmstudio
+                                                   ) 
+                else:
+                    _embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
             case "azureopenai":
                 from langchain_openai import AzureOpenAIEmbeddings
                 _embeddings = AzureOpenAIEmbeddings(deployment=os.environ["AZURE_EMBEDDING_MODEL"], chunk_size=16)
