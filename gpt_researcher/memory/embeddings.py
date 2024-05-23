974d003a1a12f60eaa6e26993ed7fd2d688b1b0c
sebaxzero <ncsebaxzero@gmail.com>
2024-05-23 15:47:56 -0400

    add custom case, set env name to OPENAI_EMBEDDING_MODEL

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -10,19 +10,16 @@ class Memory:
             case "ollama":
                 from langchain.embeddings import OllamaEmbeddings
                 _embeddings = OllamaEmbeddings(model="llama2")
-            case "openai":
+            case "custom":
                 from langchain_openai import OpenAIEmbeddings
-                base_url = os.environ.get("OPENAI_BASE_URL", None)
-                api_key = os.environ["OPENAI_API_KEY"]
-                model = os.environ.get("EMB_MODEL", "local")
-                if base_url:
-                    _embeddings = OpenAIEmbeddings(model=model,
-                                                   openai_api_key=api_key, 
-                                                   openai_api_base=base_url,
+                _embeddings = OpenAIEmbeddings(model=os.environ.get("OPENAI_EMBEDDING_MODEL", "custom"),
+                                                   openai_api_key=os.environ["OPENAI_API_KEY", "custom"], 
+                                                   openai_api_base=os.environ.get("OPENAI_BASE_URL", "http://localhost:1234/v1"), #default for lmstudio
                                                    check_embedding_ctx_length=False, #quick fix for lmstudio
                                                    ) 
-                else:
-                    _embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+            case "openai":
+                from langchain_openai import OpenAIEmbeddings
+                _embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
             case "azureopenai":
                 from langchain_openai import AzureOpenAIEmbeddings
                 _embeddings = AzureOpenAIEmbeddings(deployment=os.environ["AZURE_EMBEDDING_MODEL"], chunk_size=16)
