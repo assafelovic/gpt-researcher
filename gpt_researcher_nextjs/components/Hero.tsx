6e21dab8e7fc8ad8af16a95454093c7da2aeebb9
ElishaKay <kramer1346@gmail.com>
2024-06-05 20:57:19 +0300

    more cleanup & settings modal

--- gpt_researcher_nextjs/components/Hero.tsx
+++ gpt_researcher_nextjs/components/Hero.tsx
@@ -3,8 +3,6 @@ import { FC } from "react";
 import InputArea from "./InputArea";
 
 import logo from './logo.svg';
-import './App.css';
-import ChatBox from './ChatBox'
 
 type THeroProps = {
   promptValue: string;
@@ -23,75 +21,67 @@ const Hero: FC<THeroProps> = ({
 
   return (
     <div>
-      
-    <div className="App">
-      <header className="App-header">
-        <ChatBox />
-      </header>
-    </div>
 
-    
-    
-    <div className="flex flex-col items-center justify-center">
-      <a
-        className="mb-4 inline-flex h-7 shrink-0 items-center gap-[9px] rounded-[50px] border-[0.5px] border-solid border-[#E6E6E6] bg-white px-3 py-4 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)]"
-        href="https://github.com/assafelovic/gpt-researcher"
-        target="_blank"
-      >
-        <Image src="/img/together-ai.svg" alt="hero" width={18} height={18} />
-        <span className="text-center text-base font-light leading-[normal] text-[#1B1B16]">
-          Powered by GPT Researcher
-        </span>
-      </a>
-      <h2 className="bg-custom-gradient bg-clip-text pb-7 pt-2 text-center text-3xl font-semibold leading-[normal] lg:text-[64px]">
-        Search smarter & faster
-      </h2>
+      <div className="flex flex-col items-center justify-center">
+        <a
+          className="mb-4 inline-flex h-7 shrink-0 items-center gap-[9px] rounded-[50px] border-[0.5px] border-solid border-[#E6E6E6] bg-white px-3 py-4 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)]"
+          href="https://github.com/assafelovic/gpt-researcher"
+          target="_blank"
+        >
+          <Image src="/img/together-ai.svg" alt="hero" width={18} height={18} />
+          <span className="text-center text-base font-light leading-[normal] text-[#1B1B16]">
+            Powered by GPT Researcher
+          </span>
+        </a>
+        <h2 className="bg-custom-gradient bg-clip-text pb-7 pt-2 text-center text-3xl font-semibold leading-[normal] lg:text-[64px]">
+          Search smarter & faster
+        </h2>
 
-      {/* input section */}
-      <div className="w-full max-w-[708px] pb-6">
-        <InputArea
-          promptValue={promptValue}
-          setPromptValue={setPromptValue}
-          handleDisplayResult={handleDisplayResult}
-        />
+        {/* input section */}
+        <div className="w-full max-w-[708px] pb-6">
+          <InputArea
+            promptValue={promptValue}
+            setPromptValue={setPromptValue}
+            handleDisplayResult={handleDisplayResult}
+          />
+        </div>
+
+        {/* Suggestions section */}
+        <div className="flex flex-wrap items-center justify-center gap-2.5 pb-[30px] lg:flex-nowrap lg:justify-normal">
+          {suggestions.map((item) => (
+            <div
+              className="flex h-[35px] cursor-pointer items-center justify-center gap-[5px] rounded border border-solid border-[#C1C1C1] bg-[#EDEDEA] px-2.5 py-2"
+              onClick={() => handleClickSuggestion(item?.name)}
+              key={item.id}
+            >
+              <Image
+                src={item.icon}
+                alt={item.name}
+                width={18}
+                height={16}
+                className="w-[18px]"
+              />
+              <span className="text-sm font-light leading-[normal] text-[#1B1B16]">
+                {item.name}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        {/* Github link section */}
+        <p className="text-center text-sm font-light leading-[normal] text-[#1B1B16]">
+          Fully open source!{" "}
+          <span className="text-sm font-medium underline">
+            <a
+              href="https://github.com/assafelovic/gpt-researcher"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Star it on github.
+            </a>
+          </span>
+        </p>
       </div>
-
-      {/* Suggestions section */}
-      <div className="flex flex-wrap items-center justify-center gap-2.5 pb-[30px] lg:flex-nowrap lg:justify-normal">
-        {suggestions.map((item) => (
-          <div
-            className="flex h-[35px] cursor-pointer items-center justify-center gap-[5px] rounded border border-solid border-[#C1C1C1] bg-[#EDEDEA] px-2.5 py-2"
-            onClick={() => handleClickSuggestion(item?.name)}
-            key={item.id}
-          >
-            <Image
-              src={item.icon}
-              alt={item.name}
-              width={18}
-              height={16}
-              className="w-[18px]"
-            />
-            <span className="text-sm font-light leading-[normal] text-[#1B1B16]">
-              {item.name}
-            </span>
-          </div>
-        ))}
-      </div>
-
-      {/* Github link section */}
-      <p className="text-center text-sm font-light leading-[normal] text-[#1B1B16]">
-        Fully open source!{" "}
-        <span className="text-sm font-medium underline">
-          <a
-            href="https://github.com/assafelovic/gpt-researcher"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Star it on github.
-          </a>
-        </span>
-      </p>
-    </div>
   
     </div>
   );
---
+++ gpt_researcher_nextjs/components/Settings/Modal.js
@@ -0,0 +1,73 @@
+import React from "react";
+import './App.css';
+import ChatBox from './ChatBox'
+
+export default function Modal() {
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <>
+      <button
+        className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Search Settings
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Report Settings
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  
+                    <div className="App">
+                        <header className="App-header">
+                            <ChatBox />
+                        </header>
+                    </div>
+
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
