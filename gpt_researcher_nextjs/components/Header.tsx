aeef4afc83c66495d4f6f021d2673cf5c2ae015a
ElishaKay <kramer1346@gmail.com>
2024-06-05 21:12:33 +0300

    cleaner styling on search & settings

--- gpt_researcher_nextjs/components/Header.tsx
+++ gpt_researcher_nextjs/components/Header.tsx
@@ -6,15 +6,7 @@ const Header = () => {
       <div className="grid h-full grid-cols-12">
         <div className="col-span-5"></div>
         <div className="col-span-2 flex items-center justify-center">
-          <a href="/">
-            <Image
-              src="/img/logo.svg"
-              alt="logo"
-              width={40}
-              height={39}
-              className="h-[33px] w-[35px] lg:h-10 lg:w-10"
-            />
-          </a>
+          
         </div>
       </div>
     </div>
