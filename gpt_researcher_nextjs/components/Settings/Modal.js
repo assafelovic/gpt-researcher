3149fdc48901506968e2830322280a8b586a150c
ElishaKay <kramer1346@gmail.com>
2024-06-13 17:58:48 +0000

    green close button on settings

--- gpt_researcher_nextjs/components/Settings/Modal.js
+++ gpt_researcher_nextjs/components/Settings/Modal.js
@@ -3,6 +3,8 @@ import './App.css';
 import ChatBox from './ChatBox';
 
 export default function Modal({ setChatBoxSettings, chatBoxSettings }) {
+  console.log('chatBoxSettings',chatBoxSettings)
+
   const [showModal, setShowModal] = React.useState(false);
 
   const handleSaveChanges = () => {
@@ -52,7 +54,7 @@ export default function Modal({ setChatBoxSettings, chatBoxSettings }) {
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
