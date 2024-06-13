61abe5862becc98dbab2492a973c6cc5359b207d
ElishaKay <kramer1346@gmail.com>
2024-06-13 09:36:17 +0000

    entire gptr research report renders in new accordion components - need to listen for data.content == source_urls (list), added_source_url (append to state list) & subqueries - (see questions component on initial app load)

--- gpt_researcher_nextjs/components/Task/Accordion.tsx
+++ gpt_researcher_nextjs/components/Task/Accordion.tsx
@@ -1,26 +1,15 @@
 import React, { useState } from "react";
 
-const Accordion = () => {
+const Accordion = ({ logs }) => {
   return (
     <section className="relative z-20 overflow-hidden bg-white pb-12 pt-20 dark:bg-dark lg:pb-[90px] lg:pt-[120px]">
       <div className="container mx-auto">
-        
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 lg:w-1/2">
-            <AccordionItem
-              header="How long we deliver your first blog post?"
-              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
-            />
-            <AccordionItem
-              header="How long we deliver your first blog post?"
-              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
-            />
-            <AccordionItem
-              header="How long we deliver your first blog post?"
-              text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
-            />
+            {logs.map((log, index) => (
+              <AccordionItem key={index} header={log.header} text={log.text} />
+            ))}
           </div>
-          
         </div>
       </div>
 
