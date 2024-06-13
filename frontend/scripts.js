f9bb885f46a713daa2dbe59bd1059caeb539a78a
REF_BRUNO <ref_bruno@ANDREIA-CRIACAO.local>
2024-06-13 18:47:57 -0300

    up

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -11,19 +11,17 @@ const GPTResearcher = (() => {
       document.getElementById("output").innerHTML = "";
       document.getElementById("reportContainer").innerHTML = "";
       updateState("in_progress")
-  
+
       addAgentResponse({ output: "🤔 Thinking about research questions for the task..." });
-  
+
       listenToSockEvents();
     };
-  
+
     const listenToSockEvents = () => {
       const { protocol, host, pathname } = window.location;
       const ws_uri = `${protocol === 'https:' ? 'wss:' : 'ws:'}//${host}${pathname}ws`;
       const converter = new showdown.Converter();
       const socket = new WebSocket(ws_uri);
-      writeReport(ws_uri, converter);
-      console.log(ws_uri);
       socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
         if (data.type === 'logs') {
@@ -35,22 +33,22 @@ const GPTResearcher = (() => {
           updateDownloadLink(data);
         }
       };
-  
+
       socket.onopen = (event) => {
         const task = document.querySelector('input[name="task"]').value;
         const report_type = document.querySelector('select[name="report_type"]').value;
         const agent = document.querySelector('input[name="agent"]:checked').value;
-  
+
         const requestData = {
           task: task,
           report_type: report_type,
           agent: agent,
         };
-  
+
         socket.send(`start ${JSON.stringify(requestData)}`);
       };
     };
-  
+
     const addAgentResponse = (data) => {
       const output = document.getElementById("output");
       output.innerHTML += '<div class="agent_response">' + data.output + '</div>';
@@ -58,25 +56,25 @@ const GPTResearcher = (() => {
       output.style.display = "block";
       updateScroll();
     };
-  
+
     const writeReport = (data, converter) => {
       const reportContainer = document.getElementById("reportContainer");
       const markdownOutput = converter.makeHtml(data.output);
       reportContainer.innerHTML += markdownOutput;
       updateScroll();
     };
-  
+
     const updateDownloadLink = (data) => {
       const pdf_path = data.output.pdf;
       const docx_path = data.output.docx;
       document.getElementById("downloadLink").setAttribute("href", pdf_path);
       document.getElementById("downloadLinkWord").setAttribute("href", docx_path);
     };
-  
+
     const updateScroll = () => {
       window.scrollTo(0, document.body.scrollHeight);
     };
-  
+
     const copyToClipboard = () => {
       const textarea = document.createElement('textarea');
       textarea.id = 'temp_element';
