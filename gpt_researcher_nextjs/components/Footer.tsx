40b13628c3317afae94131ae4645af7954d74a2d
ElishaKay <kramer1346@gmail.com>
2024-06-13 17:22:14 +0000

    setup for passing report settings down from page.tsx to modal & back

--- gpt_researcher_nextjs/components/Footer.tsx
+++ gpt_researcher_nextjs/components/Footer.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import Modal from './Settings/Modal';
 
-const Footer = ({ setChatBoxSettings }) => {
+const Footer = ({ setChatBoxSettings, saveSettings }) => {
   return (
     <>
       <div className="container flex min-h-[72px] items-center justify-between border-t border-[#D2D2D2] px-4 pb-3 pt-5 lg:min-h-[72px] lg:px-0 lg:py-5">
-        <Modal setChatBoxSettings={setChatBoxSettings} />
+        <Modal setChatBoxSettings={setChatBoxSettings} saveSettings={saveSettings} />
         <div className="flex items-center gap-3">
           <Link href={"https://github.com/assafelovic/gpt-researcher"} target="_blank">
             <Image
--- gpt_researcher_nextjs/components/Task/ResearchForm.js
+++ gpt_researcher_nextjs/components/Task/ResearchForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import FileUpload from '../Settings/FileUpload';
 
-export default function ResearchForm({ onFormSubmit, defaultReportType }) {
+export default function ResearchForm({ onFormSubmit, chatBoxSettings }) {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
+    let {currentReportType, currentReportSource} = chatBoxSettings;
+
 
     return (
         <form method="POST" className="mt-3" onSubmit={onFormSubmit}>
@@ -14,7 +16,7 @@ export default function ResearchForm({ onFormSubmit, defaultReportType }) {
             <div className="form-group">
                 <label htmlFor="report_type" className="agent_question">What type of report would you like me to
                     generate?</label>
-                <select name="report_type" defaultValue={defaultReportType} className="form-control" required>
+                <select name="report_type" defaultValue={currentReportType} className="form-control" required>
                     <option value="multi_agents">Multi Agents</option>
                     <option value="research_report">Research Report</option>
                     <option value="resource_report">Resource Report</option>
@@ -23,7 +25,7 @@ export default function ResearchForm({ onFormSubmit, defaultReportType }) {
             </div>
             <div className="form-group">
                 <label htmlFor="report_source" className="agent-question">Report Source</label>
-                <select name="report_source" className="form-control" required>
+                <select name="report_source" defaultValue={currentReportSource} className="form-control" required>
                     <option value="web">The Internet</option>
                     <option value="local">My Documents</option>
                 </select>
