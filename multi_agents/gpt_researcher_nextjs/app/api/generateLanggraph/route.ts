f7e67a4bcb7281ed474120c41a666d46c03c5b19
ElishaKay <kramer1346@gmail.com>
2024-06-21 05:48:04 +0000

    multi_agents backend

--- multi_agents/gpt_researcher_nextjs/app/api/generateLanggraph/route.ts
+++ multi_agents/gpt_researcher_nextjs/app/api/generateLanggraph/route.ts
@@ -49,40 +49,6 @@ export async function POST(request: Request) {
 
     console.log('Run ID: in route.ts', run["run_id"]);
 
-    try {
-      // Poll the run until it completes
-      let finalRunStatus = await client.runs.get(thread["thread_id"], run["run_id"]);
-      while (finalRunStatus.status !== "success") {
-        await new Promise((resolve) => setTimeout(resolve, 1000)); // Polling every second
-        finalRunStatus = await client.runs.get(thread["thread_id"], run["run_id"]);
-      }
-
-      console.log('Final run status in route.ts', finalRunStatus);
-
-      // Get the final results
-      const results = await client.runs.listEvents(thread["thread_id"], run["run_id"]);
-
-      // The results are sorted by time, so the most recent (final) step is the 0 index
-      const finalResult = results[0];
-      console.log('Final result in route.ts', finalResult);
-
-      // Get the content of the final message
-      const finalMessages = (finalResult.data as Record<string, any>)["output"]["messages"];
-      const finalMessageContent = finalMessages[finalMessages.length - 1].content;
-      console.log('Final message content in route.ts', finalMessageContent);
-
-      // Ensure the response is a valid JSON string
-      if (typeof finalMessageContent === 'string') {
-        return JSON.parse(finalMessageContent);
-      } else {
-        console.error("Response is not a valid JSON string:", finalMessageContent);
-        throw new Error("Invalid JSON response");
-      }
-    } catch (error) {
-      console.error("Failed to fetch response:", error);
-      throw error;
-    }
-
     // Example usage of ChiefEditorAgent
     const chiefEditorAgent = new ChiefEditorAgent(task.task);
     await chiefEditorAgent.runResearchTask();
@@ -93,11 +59,11 @@ export async function POST(request: Request) {
 
     // Example usage of EditorAgent
     const editorAgent = new EditorAgent();
-    const researchState = {
-      initial_research: task.initial_research,
-      task: task.task
-    };
-    const researchPlan = await editorAgent.planResearch(researchState);
+    // const researchState = {
+    //   initial_research: task.initial_research,
+    //   task: task.task
+    // };
+    // const researchPlan = await editorAgent.planResearch(researchState);
 
 
     // Example usage of WriterAgent
