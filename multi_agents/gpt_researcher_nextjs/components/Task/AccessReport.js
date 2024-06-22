c62aa8de18a231dabaa2b893618d7ea5a766eec7
ElishaKay <kramer1346@gmail.com>
2024-06-22 21:18:21 +0000

    styled View as PDF & Download Docx buttons - functionality works

--- multi_agents/gpt_researcher_nextjs/components/Task/AccessReport.js
+++ multi_agents/gpt_researcher_nextjs/components/Task/AccessReport.js
@@ -17,11 +17,17 @@ export default function AccessReport({ accessData, report }) {
   };
 
   return (
-    <div>
-      <a id="downloadLink" href={getReportLink('pdf')} className="btn btn-secondary mt-3" target="_blank">
+    <div className="flex justify-end mt-4">
+      <a id="downloadLink" 
+        href={getReportLink('pdf')} 
+        className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        target="_blank">
         View as PDF
       </a>
-      <a id="downloadLink" href={getReportLink('docx')} className="btn btn-secondary mt-3" target="_blank">
+      <a id="downloadLink" 
+        href={getReportLink('docx')} 
+        className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        target="_blank">
         Download docX
       </a>
     </div>
