96a8ad3d7c94815d0efd53b19bb0bbd53259ac16
ElishaKay <kramer1346@gmail.com>
2024-06-25 03:44:41 +0000

    added transparent gptr logo to nextjs frontend

--- multi_agents/gpt_researcher_nextjs/components/Header.tsx
+++ multi_agents/gpt_researcher_nextjs/components/Header.tsx
@@ -6,7 +6,15 @@ const Header = () => {
       <div className="grid h-full grid-cols-12">
         <div className="col-span-5"></div>
         <div className="col-span-2 flex items-center justify-center">
-          
+          <a href="/">
+            <Image
+              src="/img/gptr-logo.png"
+              alt="logo"
+              width={40}
+              height={39}
+              className="h-[33px] w-[35px] lg:h-10 lg:w-10"
+            />
+          </a>
         </div>
       </div>
     </div>
