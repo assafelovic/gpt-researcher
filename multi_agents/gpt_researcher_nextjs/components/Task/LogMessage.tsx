dae8c3b955b244ffd12975ed28d58d0e948659c8
ElishaKay <kramer1346@gmail.com>
2024-06-25 04:36:13 +0000

    only show log content field when content == subquery_context window - fixes the issue of too much top padding on log message

--- multi_agents/gpt_researcher_nextjs/components/Task/LogMessage.tsx
+++ multi_agents/gpt_researcher_nextjs/components/Task/LogMessage.tsx
@@ -14,9 +14,7 @@ const LogMessage = ({ logs }) => {
               } else {
                 return (
                   <div key={index} className="mb-8 w-full rounded-lg bg-white p-4 shadow-[0px_20px_95px_0px_rgba(201,203,204,0.30)] dark:bg-dark-2 dark:shadow-[0px_20px_95px_0px_rgba(0,0,0,0.30)] sm:p-8 lg:px-6 xl:px-8">
-                    <h4 className="mt-1 text-lg font-semibold text-dark dark:text-white">
-                      {log.header}
-                    </h4>
+                    
                     <p className="py-3 text-base leading-relaxed text-body-color dark:text-dark-6">
                       {log.text}
                     </p>
