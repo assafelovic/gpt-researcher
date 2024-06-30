d01221d2b719b957bcbe5788357dfda54269f3ae
ElishaKay <kramer1346@gmail.com>
2024-06-30 12:06:42 +0000

    black text on Accordion

--- multi_agents/gpt_researcher_nextjs/components/Task/Accordion.tsx
+++ multi_agents/gpt_researcher_nextjs/components/Task/Accordion.tsx
@@ -114,7 +114,7 @@ const Accordion = ({ logs }) => {
         );
       });
     } else {
-      return <p className="mb-2 text-gray-500 dark:text-gray-400">{log.text}</p>;
+      return <p className="mb-2 text-body-color dark:text-dark-6 dark:text-gray-400">{log.text}</p>;
     }
   };
 
@@ -131,7 +131,7 @@ const Accordion = ({ logs }) => {
           <h2 id={`accordion-collapse-heading-${index}`}>
             <button
               type="button"
-              className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+              className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-body-color dark:text-dark-6 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
               onClick={() => handleToggle(index)}
               aria-expanded={openIndex === index}
               aria-controls={`accordion-collapse-body-${index}`}
