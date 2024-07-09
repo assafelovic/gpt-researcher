417e4f9ee86aa6a4bcb17c296d009e19ba7e7ffa
Andre Friedel <dreroc@users.noreply.github.com>
2024-07-08 23:03:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-exclude_packages = ["selenium", "webdriver", "fastapi", "fastapi.*", "uvicorn", "jinja2", "gpt-researcher"]
+exclude_packages = ["selenium", "webdriver", "fastapi", "fastapi.*", "uvicorn", "jinja2", "OmniResearcher"]
 
 with open(r"README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -9,16 +9,16 @@ with open("requirements.txt", "r") as f:
     reqs = [line.strip() for line in f if not any(pkg in line for pkg in exclude_packages)]
 
 setup(
-    name="gpt-researcher",
+    name="OmniResearcher",
     version="0.7.1",
-    description="GPT Researcher is an autonomous agent designed for comprehensive online research on a variety of tasks.",
+    description="OmniResearcher is an autonomous agent designed for comprehensive online research on a variety of tasks.",
     package_dir={'gpt_researcher': 'gpt_researcher'},
     packages=find_packages(exclude=exclude_packages),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/assafelovic/gpt-researcher",
-    author="Assaf Elovic",
-    author_email="assaf.elovic@gmail.com",
+    url="https://github.com/dreroc/OmniResearcher",
+    author="Mighty DreRoc",
+    author_email="andresfriedel@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
