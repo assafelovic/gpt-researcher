5d88e9777ccf10f7584413935ab8473834b4f0b4
ElishaKay <kramer1346@gmail.com>
2024-07-10 21:42:07 +0000

    polishing out more errors

--- gpt_researcher/master/agent.py
+++ gpt_researcher/master/agent.py
@@ -59,7 +59,7 @@ class GPTResearcher:
         self.report_source: str = report_source
         self.research_costs: float = 0.0
         self.cfg = Config(config_path)
-        self.retriever = get_retriever(self.cfg.retriever)
+        self.retriever = get_retriever(headers.get("retriever")) or get_retriever(self.cfg.retriever)
         self.context = context
         self.source_urls = source_urls
         self.documents = documents
