e35399cb3c1327561af98f7303b4b3996429e068
Iman Reihanian <ireihani@uci.edu>
2024-07-30 13:27:47 -0700

    feat: Add PubMed Central retriever
    This commit adds the PubMed Central retriever to the list of available retrievers in the code. The retriever allows access to the NCBI (National Center for Biotechnology Information) services, including PubMed Central and other NCBI databases. The necessary changes have been made to the code files: `gpt_researcher/retrievers/__init__.py` and `gpt_researcher/master/actions.py`. The `.env.example` file has also been updated to include a new environment variable `NCBI_API_KEY` for the API key required to access the NCBI services.

--- gpt_researcher/retrievers/__init__.py
+++ gpt_researcher/retrievers/__init__.py
@@ -1,13 +1,14 @@
-from .bing.bing import BingSearch
 from .arxiv.arxiv import ArxivSearch
+from .bing.bing import BingSearch
 from .custom.custom import CustomRetriever
 from .duckduckgo.duckduckgo import Duckduckgo
 from .google.google import GoogleSearch
+from .pubmed_central.pubmed_central import PubMedCentralSearch
 from .searx.searx import SearxSearch
+from .semantic_scholar.semantic_scholar import SemanticScholarSearch
 from .serpapi.serpapi import SerpApiSearch
 from .serper.serper import SerperSearch
 from .tavily.tavily_search import TavilySearch
-from .semantic_scholar.semantic_scholar import SemanticScholarSearch
 
 __all__ = [
     "TavilySearch",
@@ -20,4 +21,5 @@ __all__ = [
     "BingSearch",
     "ArxivSearch",
     "SemanticScholarSearch",
+    "PubMedCentralSearch",
 ]
