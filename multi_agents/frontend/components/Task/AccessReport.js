ebdc9958adcccd6924a9a525a83c88a1a3fff437
Muhammed Said Cakir <muhammedsaidckr@gmail.com>
2024-07-31 16:53:48 +0300

    Added copy to clipboard button to the access report

--- multi_agents/frontend/components/Task/AccessReport.js
+++ multi_agents/frontend/components/Task/AccessReport.js
@@ -18,14 +18,19 @@ export default function AccessReport({ accessData, report }) {
 
   return (
     <div className="flex justify-center mt-4">
-      <a id="downloadLink" 
-        href={getReportLink('pdf')} 
+      <button
+          onClick={copyToClipboard}
+          className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+          Copy to clipboard
+      </button>
+      <a id="downloadLink"
+        href={getReportLink('pdf')}
         className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         target="_blank">
         View as PDF
       </a>
-      <a id="downloadLink" 
-        href={getReportLink('docx')} 
+      <a id="downloadLink"
+        href={getReportLink('docx')}
         className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         target="_blank">
         Download DocX
