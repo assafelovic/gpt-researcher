f581ea75723a7c01cce2bcc8a21defad77b4e0e8
Assaf Elovic <assaf.elovic@gmail.com>
2024-08-19 12:51:05 +0300

    updated config for longer output articles and updated gpt model

--- gpt_researcher/config/config.py
+++ gpt_researcher/config/config.py
@@ -16,7 +16,7 @@ class Config:
         self.ollama_base_url = os.getenv('OLLAMA_BASE_URL', None)
         self.llm_model = "gpt-4o-mini"  
         self.fast_llm_model = os.getenv('FAST_LLM_MODEL', "gpt-4o-mini")
-        self.smart_llm_model = os.getenv('SMART_LLM_MODEL', "gpt-4o")
+        self.smart_llm_model = os.getenv('SMART_LLM_MODEL', "gpt-4o-2024-08-06")
         self.fast_token_limit = int(os.getenv('FAST_TOKEN_LIMIT', 2000))
         self.smart_token_limit = int(os.getenv('SMART_TOKEN_LIMIT', 4000))
         self.browse_chunk_max_length = int(os.getenv('BROWSE_CHUNK_MAX_LENGTH', 8192))
@@ -27,7 +27,7 @@ class Config:
                                                    "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0")
         self.max_search_results_per_query = int(os.getenv('MAX_SEARCH_RESULTS_PER_QUERY', 5))
         self.memory_backend = os.getenv('MEMORY_BACKEND', "local")
-        self.total_words = int(os.getenv('TOTAL_WORDS', 800))
+        self.total_words = int(os.getenv('TOTAL_WORDS', 1000))
         self.report_format = os.getenv('REPORT_FORMAT', "APA")
         self.max_iterations = int(os.getenv('MAX_ITERATIONS', 3))
         self.agent_role = os.getenv('AGENT_ROLE', None)
