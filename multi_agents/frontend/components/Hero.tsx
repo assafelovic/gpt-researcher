d36befa98a2837b8fdf303ef79a3fdd231c087d4
Assaf Elovic <assaf.elovic@gmail.com>
2024-08-21 20:26:15 +0300

    modified hero subtitle

--- multi_agents/frontend/components/Hero.tsx
+++ multi_agents/frontend/components/Hero.tsx
@@ -23,19 +23,22 @@ const Hero: FC<THeroProps> = ({
     <div>
 
       <div className="flex flex-col items-center justify-center">
-        <div className="landing">
-          <h1 className="text-4xl font-extrabold mx-auto lg:text-7xl">
-            Say Goodbye to <br/>
-            <span
-              style={{
-                backgroundImage: 'linear-gradient(to right, #9867F0, #ED4E50)',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent'
-              }}
-            >
-              Hours of Research
-            </span>
-          </h1>
+        <div className="landing flex flex-col items-center">
+            <h1 className="text-4xl font-extrabold text-center lg:text-7xl">
+              Say Goodbye to <br />
+              <span
+                style={{
+                  backgroundImage: 'linear-gradient(to right, #9867F0, #ED4E50)',
+                  WebkitBackgroundClip: 'text',
+                  WebkitTextFillColor: 'transparent',
+                }}
+              >
+                Hours of Research
+              </span>
+            </h1>
+            <h2 className="text-xl font-light text-center mb-4 ml-[20vw] mr-[20vw] mb-10">
+              Say Hello to GPT Researcher, your AI mate for rapid insights and comprehensive research
+            </h2>
         </div>
         
 
