5dc0395ef86f7cc34f196df4ad3b7cd7db39f762
Daniel Kaesmayr <daniel.kaesmayr@basf.com>
2024-08-22 12:26:56 +0200

    multi-agent approach should now use .env file for configuration of the model used (reuseing GPT-R model factory)

--- multi_agents/agents/utils/llms.py
+++ multi_agents/agents/utils/llms.py
@@ -1,15 +1,50 @@
+import json5 as json
+import json_repair
 from langchain_community.adapters.openai import convert_openai_messages
-from langchain_openai import ChatOpenAI
+
+from gpt_researcher.config.config import Config
+from gpt_researcher.master.actions import handle_json_error
+from gpt_researcher.utils.llm import create_chat_completion
+
+from loguru import logger
 
 
-async def call_model(prompt: list, model: str, max_retries: int = 2, response_format: str = None, api_key: str = None) -> str:
+async def call_model(
+    prompt: list,
+    model: str,
+    response_format: str = None,
+):
 
     optional_params = {}
-    if response_format == 'json':
-        optional_params = {
-            "response_format": {"type": "json_object"}
-        }
+    if response_format == "json":
+        optional_params = {"response_format": {"type": "json_object"}}
 
+    cfg = Config()
     lc_messages = convert_openai_messages(prompt)
-    response = ChatOpenAI(model=model, max_retries=max_retries, model_kwargs=optional_params, api_key=api_key).invoke(lc_messages).content
-    return response
+
+    try:
+        response = await create_chat_completion(
+            model=model,
+            messages=lc_messages,
+            temperature=0,
+            llm_provider=cfg.llm_provider,
+            llm_kwargs=cfg.llm_kwargs,
+            # cost_callback=cost_callback,
+        )
+
+        if response_format == "json":
+            try:
+                cleaned_json_string = response.strip("```json\n")
+                return json.loads(cleaned_json_string)
+            except Exception as e:
+                print("⚠️ Error in reading JSON, attempting to repair JSON")
+                logger.error(
+                    f"Error in reading JSON, attempting to repair reponse: {response}"
+                )
+                return json_repair.loads(response)
+        else:
+            return response
+
+    except Exception as e:
+        print("⚠️ Error in calling model")
+        logger.error(f"Error in calling model: {e}")
