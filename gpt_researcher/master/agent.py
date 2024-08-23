8aa2fd52d426830c0cef550cb02c44ec9cc56adf
Alex Lee <oneyedjack2021@gmail.com>
2024-08-23 10:04:20 -0700

    remove param

--- gpt_researcher/master/agent.py
+++ gpt_researcher/master/agent.py
@@ -618,7 +618,6 @@ class GPTResearcher:
             parent_query=self.parent_query,
             report_type=self.report_type,
             cost_callback=self.add_costs,
-            openai_api_key=self.headers.get("openai_api_key"),
         )
     
     async def get_similar_written_contents_by_draft_section_titles(
