828ca42711dbbba8d9e83fb7ad3f77b93029af7a
Assaf Elovic <assaf.elovic@gmail.com>
2024-09-13 10:09:39 +0300

    updated prompts

--- gpt_researcher/master/prompts.py
+++ gpt_researcher/master/prompts.py
@@ -29,7 +29,8 @@ def generate_search_queries_prompt(
         task = question
 
     return (
-        f'Write {max_iterations} google search queries to search online that form an objective opinion from the following task: "{task}"'
+        f'Write {max_iterations} google search queries to search online that form an objective opinion from the following task: "{task}"\n'
+        f'Assume the current date is {datetime.now(timezone.utc).strftime('%B %d, %Y')} if required.\n'
         f'You must respond with a list of strings in the following format: ["query 1", "query 2", "query 3"].\n'
         f"The response should contain ONLY the list."
     )
