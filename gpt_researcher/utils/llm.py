6d7424e0b36ab8c212a5ffaf673a007ff377c2a9
Martin Lund Haug <martinlhaug@gmail.com>
2024-10-12 12:21:59 +0200

    update doc string to reflect the actual default values

--- gpt_researcher/utils/llm.py
+++ gpt_researcher/utils/llm.py
@@ -34,8 +34,8 @@ async def create_chat_completion(
     Args:
         messages (list[dict[str, str]]): The messages to send to the chat completion
         model (str, optional): The model to use. Defaults to None.
-        temperature (float, optional): The temperature to use. Defaults to 0.9.
-        max_tokens (int, optional): The max tokens to use. Defaults to None.
+        temperature (float, optional): The temperature to use. Defaults to 0.4.
+        max_tokens (int, optional): The max tokens to use. Defaults to 4000.
         stream (bool, optional): Whether to stream the response. Defaults to False.
         llm_provider (str, optional): The LLM Provider to use.
         webocket (WebSocket): The websocket used in the currect request,
