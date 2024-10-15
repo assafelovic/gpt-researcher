7fb80905343a0bd001c42491426e84ed5c2fb493
Assaf Elovic <assaf.elovic@gmail.com>
2024-10-15 15:09:31 +0300

    fixed bug with releative paths in new pip

--- gpt_researcher/config/__init__.py
+++ gpt_researcher/config/__init__.py
@@ -1,3 +1,6 @@
 from .config import Config
+from .variables.base import BaseConfig
+from .variables.default import DefaultConfig
+from .variables.retrievers import RetrieversConfig
 
-__all__ = ["Config"]
+__all__ = ["Config", "BaseConfig", "DefaultConfig", "RetrieversConfig"]
