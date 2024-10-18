7019625ea7b30a657bc6e2c9c82c1d434a35beef
Assaf Elovic <assaf.elovic@gmail.com>
2024-10-18 19:54:17 +0300

    moved agent creator

--- gpt_researcher/actions/__init__.py
+++ gpt_researcher/actions/__init__.py
@@ -1,5 +1,6 @@
 from .retriever import get_retriever, get_retrievers
-from .query_processing import get_sub_queries, extract_json_with_regex, choose_agent
+from .query_processing import get_sub_queries
+from .agent_creator import extract_json_with_regex, choose_agent
 from .web_scraping import scrape_urls
 from .report_generation import write_conclusion, summarize_url, generate_draft_section_titles, generate_report, write_report_introduction
 from .markdown_processing import extract_headers, extract_sections, table_of_contents, add_references
