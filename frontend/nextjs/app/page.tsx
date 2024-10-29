f574802340627e7f2d6c47879a7b40f183b3b996
Assaf Elovic <assaf.elovic@gmail.com>
2024-10-29 21:00:54 +0200

    modified margins

--- frontend/nextjs/app/page.tsx
+++ frontend/nextjs/app/page.tsx
@@ -395,7 +395,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="h-full px-4 pb-4 pt-10">
+      <main className="h-full px-4 pb-4">
         {!showResult && (
           <Hero
             promptValue={promptValue}
