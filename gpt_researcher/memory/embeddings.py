c97c4df933792e7816779714a028dfc300d261c6
Tan, Kok Meng <kesamet@gmail.com>
2024-10-30 17:49:18 +0800

    Add embedding providers

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -1,13 +1,23 @@
 import os
 from typing import Any
 
-OPENAI_EMBEDDING_MODEL = os.environ.get("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small")
+OPENAI_EMBEDDING_MODEL = os.environ.get(
+    "OPENAI_EMBEDDING_MODEL", "text-embedding-3-small"
+)
 
 _SUPPORTED_PROVIDERS = {
     "openai",
     "azure_openai",
+    "cohere",
+    "google_vertexai",
+    "google_genai",
+    "fireworks",
     "ollama",
+    "together",
+    "mistralai",
     "huggingface",
+    "nomic",
+    "voyageai",
     "custom",
 }
 
@@ -16,14 +26,6 @@ class Memory:
     def __init__(self, embedding_provider: str, model: str, **embdding_kwargs: Any):
         _embeddings = None
         match embedding_provider:
-            case "ollama":
-                from langchain_ollama import OllamaEmbeddings
-
-                _embeddings = OllamaEmbeddings(
-                    model=model,
-                    base_url=os.environ["OLLAMA_BASE_URL"],
-                    **embdding_kwargs,
-                )
             case "custom":
                 from langchain_openai import OpenAIEmbeddings
 
@@ -50,12 +52,56 @@ class Memory:
                     openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
                     **embdding_kwargs,
                 )
+            case "cohere":
+                from langchain_cohere import CohereEmbeddings
+
+                _embeddings = CohereEmbeddings(model=model, **embdding_kwargs)
+            case "google_vertexai":
+                from langchain_google_vertexai import VertexAIEmbeddings
+
+                _embeddings = VertexAIEmbeddings(model=model, **embdding_kwargs)
+            case "google_genai":
+                from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+                _embeddings = GoogleGenerativeAIEmbeddings(
+                    model=model, **embdding_kwargs
+                )
+            case "fireworks":
+                from langchain_fireworks import FireworksEmbeddings
+
+                _embeddings = FireworksEmbeddings(model=model, **embdding_kwargs)
+            case "ollama":
+                from langchain_ollama import OllamaEmbeddings
+
+                _embeddings = OllamaEmbeddings(
+                    model=model,
+                    base_url=os.environ["OLLAMA_BASE_URL"],
+                    **embdding_kwargs,
+                )
+            case "together":
+                from langchain_together import TogetherEmbeddings
+
+                _embeddings = TogetherEmbeddings(model=model, **embdding_kwargs)
+            case "mistralai":
+                from langchain_mistralai import MistralAIEmbeddings
+
+                _embeddings = MistralAIEmbeddings(model=model, **embdding_kwargs)
             case "huggingface":
                 from langchain_huggingface import HuggingFaceEmbeddings
 
-                # Specifying the Hugging Face embedding model sentence-transformers/all-MiniLM-L6-v2
                 _embeddings = HuggingFaceEmbeddings(model_name=model, **embdding_kwargs)
+            case "nomic":
+                from langchain_nomic import NomicEmbeddings
 
+                _embeddings = NomicEmbeddings(model=model, **embdding_kwargs)
+            case "voyageai":
+                from langchain_voyageai import VoyageAIEmbeddings
+
+                _embeddings = VoyageAIEmbeddings(
+                    voyage_api_key=os.environ["VOYAGE_API_KEY"],
+                    model=model,
+                    **embdding_kwargs,
+                )
             case _:
                 raise Exception("Embedding not found.")
 
