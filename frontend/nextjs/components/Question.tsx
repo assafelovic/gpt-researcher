d913b5da375b7624b6d1a8ee93e8ebce2692a01b
Assaf Elovic <assaf.elovic@gmail.com>
2024-11-01 17:39:58 +0200

    modified research task title

--- frontend/nextjs/components/Question.tsx
+++ frontend/nextjs/components/Question.tsx
@@ -16,7 +16,7 @@ const Question: React.FC<QuestionProps> = ({ question }) => {
           className="w-6 h-6"
         />
         <p className="font-bold uppercase leading-[152%] text-white">
-          Question:
+          Research Task:
         </p><br/>
       </div>
       <div className="grow text-white break-words max-w-full log-message">&quot;{question}&quot;</div>
