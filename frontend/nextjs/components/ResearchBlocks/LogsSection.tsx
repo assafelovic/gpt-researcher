44d162dc4c599d041e55cf9d87ddd788052407cc
Assaf Elovic <assaf.elovic@gmail.com>
2024-11-01 21:56:13 +0200

    refactored image component

--- frontend/nextjs/components/ResearchBlocks/LogsSection.tsx
+++ frontend/nextjs/components/ResearchBlocks/LogsSection.tsx
@@ -12,13 +12,13 @@ interface OrderedLogsProps {
   logs: Log[];
 }
 
-const OrderedLogs = ({ logs }: OrderedLogsProps) => {
+const LogsSection = ({ logs }: OrderedLogsProps) => {
   return (
     <div className="container h-auto w-full shrink-0 rounded-lg border border-solid border-[#C2C2C2] bg-gray-800 shadow-md p-5">
       <div className="flex items-start gap-4 pb-3 lg:pb-3.5">
         <Image src="/img/thinking.svg" alt="logs" width={24} height={24} />
         <h3 className="text-base font-bold uppercase leading-[152.5%] text-white">
-          Agent Work:
+          Agent Work
         </h3>
       </div>
       <div className="overflow-y-auto min-h-[200px] max-h-[500px] scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-300">
@@ -28,4 +28,4 @@ const OrderedLogs = ({ logs }: OrderedLogsProps) => {
   );
 };
 
-export default OrderedLogs; 
+export default LogsSection; 
