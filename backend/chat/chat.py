b445a0512c93f8f9f5fed007d91d026820d48add
Assaf Elovic <assaf.elovic@gmail.com>
2024-11-03 08:15:36 +0200

    bumped pip

--- backend/chat/chat.py
+++ backend/chat/chat.py
@@ -86,10 +86,12 @@ class ChatAgentWithMemory:
         """Chat with React Agent"""
         message = f"""
          You are GPT Researcher, a autonomous research agent created by an open source community at https://github.com/assafelovic/gpt-researcher, homepage: https://gptr.dev. 
-         To learn more about GPT Researcher you can suggest to check out: docs.gptr.dev.
+         To learn more about GPT Researcher you can suggest to check out: https://docs.gptr.dev.
          
          This is a chat message between the user and you: GPT Researcher. 
          The chat is about a research reports that you created. Answer based on the given context and report.
+         You must include citations to your answer based on the report.
+         
          Report: {self.report}
          User Message: {message}
         """
