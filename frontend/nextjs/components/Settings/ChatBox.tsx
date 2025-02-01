520108aa89ec32b4c459485ecfd22d54daa3aa30
ElishaKay <kramer1346@gmail.com>
2025-02-01 20:14:58 +0000

    add support for NEXT_PUBLIC_GPTR_API_URL env variable

--- frontend/nextjs/components/Settings/ChatBox.tsx
+++ frontend/nextjs/components/Settings/ChatBox.tsx
@@ -3,6 +3,7 @@ import ResearchForm from '../Task/ResearchForm';
 import Report from '../Task/Report';
 import AgentLogs from '../Task/AgentLogs';
 import AccessReport from '../ResearchBlocks/AccessReport';
+import { getHost } from '../../helpers/getHost';
 
 interface ChatBoxSettings {
   report_source: string;
@@ -36,8 +37,10 @@ export default function ChatBox({ chatBoxSettings, setChatBoxSettings }: ChatBox
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const { protocol, pathname } = window.location;
-      let { host } = window.location;
-      host = host.includes('localhost') ? 'localhost:8000' : host;
+      
+      const fullHost = getHost()
+      const host = fullHost.replace('http://', '').replace('https://', '')
+      
       const ws_uri = `${protocol === 'https:' ? 'wss:' : 'ws:'}//${host}${pathname}ws`;
       const newSocket = new WebSocket(ws_uri);
       setSocket(newSocket);
--- frontend/nextjs/helpers/getHost.ts
+++ frontend/nextjs/helpers/getHost.ts
@@ -5,7 +5,9 @@ interface GetHostParams {
 export const getHost = ({ purpose }: GetHostParams = {}): string => {
   if (typeof window !== 'undefined') {
     let { host } = window.location;
-    if (purpose === 'langgraph-gui') {
+    if (process.env.NEXT_PUBLIC_GPTR_API_URL) {
+      return process.env.NEXT_PUBLIC_GPTR_API_URL;
+    } else if (purpose === 'langgraph-gui') {
       return host.includes('localhost') ? 'http%3A%2F%2F127.0.0.1%3A8123' : `https://${host}`;
     } else {
       return host.includes('localhost') ? 'http://localhost:8000' : `https://${host}`;
