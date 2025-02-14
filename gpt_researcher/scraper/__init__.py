14ec4804fb5c6aa2d90f5308e6d1a411391493af
Xian Zhang <xian.z512@gmail.com>
2025-02-13 17:46:38 -0800

    feat: add nodriver scrapper

--- gpt_researcher/scraper/__init__.py
+++ gpt_researcher/scraper/__init__.py
@@ -1,9 +1,9 @@
-
 from .beautiful_soup.beautiful_soup import BeautifulSoupScraper
 from .web_base_loader.web_base_loader import WebBaseLoaderScraper
 from .arxiv.arxiv import ArxivScraper
 from .pymupdf.pymupdf import PyMuPDFScraper
 from .browser.browser import BrowserScraper
+from .browser.nodriver_scraper import NoDriverScraper
 from .tavily_extract.tavily_extract import TavilyExtract
 from .scraper import Scraper
 
@@ -13,6 +13,7 @@ __all__ = [
     "ArxivScraper",
     "PyMuPDFScraper",
     "BrowserScraper",
+    "NoDriverScraper",
     "TavilyExtract",
-    "Scraper"
+    "Scraper",
 ]
