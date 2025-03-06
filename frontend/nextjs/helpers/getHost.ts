ad7aef456b6541ca42344881419a6696d37a30cd
ElishaKay <kramer1346@gmail.com>
2025-03-06 09:52:09 +0000

    setup for modular css and fetching gptr api url from props of react component

--- frontend/nextjs/helpers/getHost.ts
+++ frontend/nextjs/helpers/getHost.ts
@@ -6,7 +6,7 @@ export const getHost = ({ purpose }: GetHostParams = {}): string => {
   if (typeof window !== 'undefined') {
     let { host } = window.location;
     if (process.env.REACT_APP_GPTR_API_URL || process.env.NEXT_PUBLIC_GPTR_API_URL) {
-      return process.env.REACT_APP_GPTR_API_URL || process.env.NEXT_PUBLIC_GPTR_API_URL;
+      return process.env.REACT_APP_GPTR_API_URL || process.env.NEXT_PUBLIC_GPTR_API_URL || '';
     } else if (purpose === 'langgraph-gui') {
       return host.includes('localhost') ? 'http%3A%2F%2F127.0.0.1%3A8123' : `https://${host}`;
     } else {
