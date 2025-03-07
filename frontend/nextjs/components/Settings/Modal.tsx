b9246f666d10fd05ee1903042e955285e0a5f0fd
Assaf Elovic <assaf.elovic@gmail.com>
2025-03-07 17:04:50 +0200

    modified

--- frontend/nextjs/components/Settings/Modal.tsx
+++ frontend/nextjs/components/Settings/Modal.tsx
@@ -77,7 +77,7 @@ const Modal: React.FC<ChatBoxProps> = ({ chatBoxSettings, setChatBoxSettings }) 
                 </div>
                 <div className="flex items-center justify-end p-3">
                   <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-purple-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={handleSaveChanges}
                   >
