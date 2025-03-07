46a5d6c4e4119f42e817b79daa5cb30e2cd86765
Assaf Elovic <assaf.elovic@gmail.com>
2025-03-07 16:15:16 +0200

    added new sidebar experience

--- frontend/nextjs/components/Settings/Modal.tsx
+++ frontend/nextjs/components/Settings/Modal.tsx
@@ -50,7 +50,7 @@ const Modal: React.FC<ChatBoxProps> = ({ chatBoxSettings, setChatBoxSettings }) 
   return (
     <div className="settings">
       <button
-        className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="bg-purple-500 text-white active:bg-purple-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         type="button"
         onClick={() => setShowModal(true)}
       >
