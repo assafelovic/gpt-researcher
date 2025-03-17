3e10bfa4b8cb73b8167e8b4b9d2b26521763472d
Kenny Dizi <xamarindevelopervietnam@outlook.com>
2025-03-17 21:09:44 +0700

    Declare NO_SUPPORT_TEMPERATURE_MODELS

--- gpt_researcher/llm_provider/generic/base.py
+++ gpt_researcher/llm_provider/generic/base.py
@@ -27,6 +27,17 @@ _SUPPORTED_PROVIDERS = {
     "gigachat"
 }
 
+NO_SUPPORT_TEMPERATURE_MODELS = [
+    "deepseek/deepseek-reasoner",
+    "o1-mini",
+    "o1-mini-2024-09-12",
+    "o1",
+    "o1-2024-12-17",
+    "o3-mini",
+    "o3-mini-2025-01-31",
+    "o1-preview"
+]
+
 SUPPORT_REASONING_EFFORT_MODELS = [
     "o3-mini",
     "o3-mini-2025-01-31"
