08fd94cb2a3c317d244f60b67863702c8d6a72c3
Assaf Elovic <assaf.elovic@gmail.com>
2025-04-04 21:51:37 +0300

    modified

--- frontend/nextjs/components/ResearchBlocks/elements/InputArea.tsx
+++ frontend/nextjs/components/ResearchBlocks/elements/InputArea.tsx
@@ -37,7 +37,7 @@ const InputArea: FC<TInputAreaProps> = ({
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const placeholder = handleSecondary
     ? "Any questions about this report?"
-    : "What would you like to research next?";
+    : "What would you like me to research next?";
 
   const resetHeight = () => {
     if (textareaRef.current) {
