11749fd8b4d5be6babc76322a6a3e06f630fe85a
Assaf Elovic <assaf.elovic@gmail.com>
2025-04-04 21:48:57 +0300

    modified

--- frontend/nextjs/components/Footer.tsx
+++ frontend/nextjs/components/Footer.tsx
@@ -31,6 +31,21 @@ const Footer: React.FC<FooterProps> = ({ chatBoxSettings, setChatBoxSettings }) 
             © {new Date().getFullYear()} GPT Researcher. All rights reserved.
         </div>
         <div className="flex items-center gap-4 order-1 sm:order-2 mb-2 sm:mb-0">
+          <Link href={"https://gptr.dev"} target="_blank" className="p-1">
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="2" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                className="w-6 h-6 sm:w-7 sm:h-7 text-white hover:text-teal-400 transition-colors duration-300"
+              >
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                <polyline points="9 22 9 12 15 12 15 22" />
+              </svg>
+          </Link>
           <Link href={"https://github.com/assafelovic/gpt-researcher"} target="_blank" className="p-1">
             <img
               src={"/img/github.svg"}
@@ -56,15 +71,6 @@ const Footer: React.FC<FooterProps> = ({ chatBoxSettings, setChatBoxSettings }) 
                 width={24}
                 height={24}
                 className="w-6 h-6 sm:w-7 sm:h-7"
-              />{" "}
-          </Link>
-          <Link href={"https://gptr.dev"} target="_blank" className="p-1">
-              <img
-                src={"/img/home.svg"}
-                alt="homepage"
-                width={24}
-                height={24}
-                className="w-6 h-6 sm:w-7 sm:h-7 filter invert brightness-100"
               />{" "}
           </Link>
         </div>
