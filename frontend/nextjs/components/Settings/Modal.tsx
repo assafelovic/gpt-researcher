3dfa7b6f6e6e92291460f002992cba9ca61e989f
Assaf Elovic <assaf.elovic@gmail.com>
2025-04-04 19:46:04 +0300

    modified ahref colors

--- frontend/nextjs/components/Settings/Modal.tsx
+++ frontend/nextjs/components/Settings/Modal.tsx
@@ -65,7 +65,7 @@ const Modal: React.FC<ChatBoxProps> = ({ chatBoxSettings, setChatBoxSettings }) 
       </button>
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[9999] outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="relative p-6 flex-auto">
@@ -94,7 +94,7 @@ const Modal: React.FC<ChatBoxProps> = ({ chatBoxSettings, setChatBoxSettings }) 
               </div>
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          <div className="opacity-25 fixed inset-0 z-[9998] bg-black"></div>
         </>
       ) : null}
     </div>
