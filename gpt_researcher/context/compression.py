3bf50bf21c280176f126aaf15926cc80d3151d9f
Gabe Goodhart <ghart@us.ibm.com>
2025-04-07 12:54:49 -0600

    feat: Use prompt family for document pretty printing
    Different models have different prompting strategies for context docs, and
    granite in particular expects to receive its documents in a standard format
    that doesn't match the basic concatenation scheme.
    
    Branch: Granite
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- gpt_researcher/context/compression.py
+++ gpt_researcher/context/compression.py
@@ -13,35 +13,46 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from ..vector_store import VectorStoreWrapper
 from ..utils.costs import estimate_embedding_cost
 from ..memory.embeddings import OPENAI_EMBEDDING_MODEL
+from ..prompts import PromptFamily
 
 
 class VectorstoreCompressor:
-    def __init__(self, vector_store: VectorStoreWrapper, max_results:int = 7, filter: Optional[dict] = None, **kwargs):
+    def __init__(
+        self,
+        vector_store: VectorStoreWrapper,
+        max_results:int = 7,
+        filter: Optional[dict] = None,
+        prompt_family: type[PromptFamily] | PromptFamily = PromptFamily,
+        **kwargs,
+    ):
 
         self.vector_store = vector_store
         self.max_results = max_results
         self.filter = filter
         self.kwargs = kwargs
-
-    def __pretty_print_docs(self, docs):
-        return f"\n".join(f"Source: {d.metadata.get('source')}\n"
-                          f"Title: {d.metadata.get('title')}\n"
-                          f"Content: {d.page_content}\n"
-                          for d in docs)
+        self.prompt_family = prompt_family
 
     async def async_get_context(self, query, max_results=5):
         """Get relevant context from vector store"""
         results = await self.vector_store.asimilarity_search(query=query, k=max_results, filter=self.filter)
-        return self.__pretty_print_docs(results)
+        return self.prompt_family.pretty_print_docs(results)
 
 
 class ContextCompressor:
-    def __init__(self, documents, embeddings, max_results=5, **kwargs):
+    def __init__(
+        self,
+        documents,
+        embeddings,
+        max_results=5,
+        prompt_family: type[PromptFamily] | PromptFamily = PromptFamily,
+        **kwargs,
+    ):
         self.max_results = max_results
         self.documents = documents
         self.kwargs = kwargs
         self.embeddings = embeddings
         self.similarity_threshold = os.environ.get("SIMILARITY_THRESHOLD", 0.35)
+        self.prompt_family = prompt_family
 
     def __get_contextual_retriever(self):
         splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
@@ -58,18 +69,12 @@ class ContextCompressor:
         )
         return contextual_retriever
 
-    def __pretty_print_docs(self, docs, top_n):
-        return f"\n".join(f"Source: {d.metadata.get('source')}\n"
-                          f"Title: {d.metadata.get('title')}\n"
-                          f"Content: {d.page_content}\n"
-                          for i, d in enumerate(docs) if i < top_n)
-
     async def async_get_context(self, query, max_results=5, cost_callback=None):
         compressed_docs = self.__get_contextual_retriever()
         if cost_callback:
             cost_callback(estimate_embedding_cost(model=OPENAI_EMBEDDING_MODEL, docs=self.documents))
         relevant_docs = await asyncio.to_thread(compressed_docs.invoke, query)
-        return self.__pretty_print_docs(relevant_docs, max_results)
+        return self.prompt_family.pretty_print_docs(relevant_docs, max_results)
 
 
 class WrittenContentCompressor:
