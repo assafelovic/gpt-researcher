e9b48a9d3bb9cd0eaba6fa6cce89757a0567c577
Assaf Elovic <assaf.elovic@gmail.com>
2025-05-29 09:17:21 +0300

    fixed sidebar error

--- frontend/nextjs/components/ResearchSidebar.tsx
+++ frontend/nextjs/components/ResearchSidebar.tsx
@@ -199,7 +199,10 @@ const ResearchSidebar: React.FC<ResearchSidebarProps> = ({
                               <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5 mr-1 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                               </svg>
-                              {formatDistanceToNow(new Date(item.timestamp), { addSuffix: true })}
+                              {item.timestamp && !isNaN(item.timestamp) 
+                                ? formatDistanceToNow(new Date(item.timestamp), { addSuffix: true })
+                                : 'Unknown time'
+                              }
                             </p>
                           </button>
                           
